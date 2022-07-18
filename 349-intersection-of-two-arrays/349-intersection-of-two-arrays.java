f0c45b0db889b176a83e0bf25ab606c4707d2129
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-07-18 16:52:51 +0200

    Time: 4 ms (72.72%), Space: 43.5 MB (77.89%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -2,25 +2,28 @@ class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int length1 = nums1.length, length2 = nums2.length;
-        int[] intersection = new int[length1 + length2];
-        int index = 0, index1 = 0, index2 = 0;
-        while (index1 < length1 && index2 < length2) {
-            int num1 = nums1[index1], num2 = nums2[index2];
-            if (num1 == num2) {
-                // 保证加入元素的唯一性
-                if (index == 0 || num1 != intersection[index - 1]) {
-                    intersection[index++] = num1;
+        int p1 = 0, p2 = 0, l1 = nums1.length, l2 = nums2.length;
+        int[] ans = new int[l1 + l2];
+        int index = 0;
+        while (p1 < l1 && p2 < l2) {
+            if (nums1[p1] == nums2[p2]) {
+                if (index == 0 || nums1[p1] != ans[index - 1]) {
+                    ans[index] = nums1[p1];
+                index++;
                 }
-                index1++;
-                index2++;
-            } else if (num1 < num2) {
-                index1++;
+                p1++;
+                p2++;
             } else {
-                index2++;
+                if (nums1[p1] < nums2[p2]) {
+                    p1++;
+                } else {
+                    p2++;
+                }
             }
         }
-        return Arrays.copyOfRange(intersection, 0, index);
+        return Arrays.copyOfRange(ans, 0, index);
     }
 }
 
+//tc: O(mlogm + nlogn)
+//sc: O(m + n)

bf32b0edcc64029ab09ec23d365885e2957bed63
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-07-18 22:04:04 +0200

    Time: 2 ms (79.19%), Space: 42.8 MB (79.39%) - LeetHub

---
+++ 969-pancake-sorting/969-pancake-sorting.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> pancakeSort(int[] arr) {
+        int N = arr.length;
+        List<Integer> ans = new ArrayList();
+        for (int pos = N - 1; pos >= 0; pos--) {
+            int i = 0;
+            while (i < pos && arr[i] != pos + 1) {
+                i++;
+            }
+            if (i != pos) {
+                if (i != 0) {
+                    reverse(arr, 0, i);
+                ans.add(i + 1);
+                } 
+                reverse(arr, 0, pos);
+                ans.add(pos + 1);
+            }
+        }
+        return ans;
+    }
+    
+    private void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}

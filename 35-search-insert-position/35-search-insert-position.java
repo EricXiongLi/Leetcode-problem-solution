e8331f75ebde4dc0c956c4ec20e7d49b9b027535
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-07-22 15:49:22 +0200

    Time: 0 ms (100.00%), Space: 43.5 MB (42.42%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + r >> 1;
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        
+        return l;
+    }
+}

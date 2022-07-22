691e51c59146102b1f76e299ee6390d799ee908a
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-07-22 09:43:35 +0200

    Time: 4 ms (21.83%), Space: 57.5 MB (71.16%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,20 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
-        for (int i = 0, product = 1; i < n; i++) {
-            ans[i] = product;
-            product *= nums[i];
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = nums[0];
+        right[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i];
+            right[n - 1 - i] = right[n - i] * nums[n - 1 - i]; 
         }
         
-        for (int i = n - 1, product = 1; i >= 0; i--) {
-            ans[i] *= product;
-            product *= nums[i];
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                ans[i] = right [i + 1];
+            } else if (i == n - 1) {
+                ans[i] = left[i - 1];
+            } else {
+                ans[i] = left[i - 1] * right[i + 1];
+            }
         }
         
         return ans;
     }
 }
-
-//tc: O(n)
-//sc: O(1)

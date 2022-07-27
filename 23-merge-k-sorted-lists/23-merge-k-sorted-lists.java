b3fcb125be308e2ef98e9141a9fb6fb233389f2c
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-07-27 18:28:24 +0200

    Time: 2 ms (98.47%), Space: 44.4 MB (86.41%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -1,23 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
-        for (ListNode node: lists) {
-            if (node != null) {
-                pq.offer(node);
-            }
+        return partitionMerge(lists, 0, lists.length - 1);
+    }
+    
+    public ListNode partitionMerge(ListNode[] lists, int start, int end) {
+        if (start == end) return lists[start];
+        if (start > end) return null;
+        int mid = (start + end) >> 1;
+        ListNode l1 = partitionMerge(lists, start, mid);
+        ListNode l2 = partitionMerge(lists, mid + 1, end);
+        return merge(l1, l2);
+    }
+    
+    public ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
         }
-        ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;
-        while (pq.size() != 0) {
-            ListNode top = pq.poll();
-            cur.next = top;
-            cur = cur.next;
-            if (pq.size() == 0) break;
-            if (top.next != null) pq.offer(top.next);
-        }
-        return dummy.next;
     }
 }
-
-//tc: O(nlogn)
-//sc: O(k)

eeb4b01ac9f4dea38eae324d13e881c246e8ac04
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-07-28 15:15:03 +0200

    Time: 1 ms (99.80%), Space: 44.8 MB (86.69%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,40 +1,18 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> ans = new ArrayList();
-        
-        for (int i = 0; i < intervals.length; i++) {
-            if (newInterval[1] < intervals[i][0]) {
-                ans.add(newInterval);
-                for (int j = i; j < intervals.length; j++) {
-                    ans.add(intervals[j]);
-                }
-                int[][] res = new int[ans.size()][2];
-                for (int k = 0; k < ans.size(); k++) {
-                    res[k][0] = ans.get(k)[0];
-                    res[k][1] = ans.get(k)[1];
-                }
-                return res;
-            }
-            else if (newInterval[0] > intervals[i][1]) {
-                ans.add(intervals[i]);
-            }
-            else {
-                newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-                newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+        List<int[]> res = new LinkedList<>();
+        for (int[] i : intervals) {
+            if (i[1] < newInterval[0]) {
+                res.add(i);
+            } else if (i[0] > newInterval[1]) {
+                res.add(newInterval);
+                newInterval = i;
+            } else {
+                newInterval[0] = Math.min(i[0], newInterval[0]);
+                newInterval[1] = Math.max(i[1], newInterval[1]);
             }
         }
-        
-        ans.add(newInterval);
-        int[][] res = new int[ans.size()][2];
-                for (int k = 0; k < ans.size(); k++) {
-                    res[k][0] = ans.get(k)[0];
-                    res[k][1] = ans.get(k)[1];
-                }
-        return res;
-        
-        
+        res.add(newInterval);
+        return res.toArray(new int[res.size()][]);
     }
 }
-
-//tc: O(n)
-//sc: O(1)

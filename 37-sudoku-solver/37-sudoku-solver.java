3aa931b4d0f8b0f0f50d5f493cc0efc96bc6ed98
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-04 11:17:00 +0200

    Time: 4 ms (97.28%), Space: 41.4 MB (72.96%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -1,49 +1,47 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        Map<Integer, Set<Character>> rowUsed = new HashMap<>();
-        Map<Integer, Set<Character>> colUsed = new HashMap<>();
-        Map<Integer, Set<Character>> blockUsed = new HashMap<>();
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                char c = board[row][col];
-                int blockNum = 3 * (row / 3) + col / 3;
-                rowUsed.computeIfAbsent(row, v -> new HashSet<Character>()).add(c);
-                colUsed.computeIfAbsent(col, v -> new HashSet<Character>()).add(c);
-                blockUsed.computeIfAbsent(blockNum, v -> new HashSet<Character>()).add(c);
+        int[][] rowUsed = new int[9][10];
+        int[][] colUsed = new int[9][10];
+        int[][] boxUsed = new int[9][10];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int box = 3 * (i / 3) + j / 3;
+                    int num = board[i][j] - '0';
+                    colUsed[j][num] = 1;
+                    rowUsed[i][num] = 1;
+                    boxUsed[box][num] = 1;
+                }
             }
         }
-        dfs(board, 0, 0, rowUsed, colUsed, blockUsed);
+        dfs(0, 0, rowUsed, colUsed, boxUsed, board);
         return;
     }
     
-    public boolean dfs(char[][] board, int row, int col, Map<Integer, Set<Character>> rowUsed, Map<Integer, Set<Character>> colUsed, Map<Integer, Set<Character>> blockUsed) {
-        //ending case
-        if (row == 9 && col == 0) {
+    private boolean dfs(int row, int col, int[][] rowUsed, int[][] colUsed, int[][] boxUsed, char[][] board) {
+        if (row == 9) {
             return true;
         }
+        int box = col / 3 + (row / 3) * 3; 
+        int nc = (col + 1) % 9;
+        int nr = nc == 0 ? row + 1 : row;
         if (board[row][col] != '.') {
-            if (col == 8) {
-                if (dfs(board, row + 1, 0, rowUsed, colUsed, blockUsed)) return true;
-            } else {
-               if (dfs(board, row, col + 1, rowUsed, colUsed, blockUsed)) return true;
-            }
-        } else {
-            for (char c = '1'; c <= '9'; c++) {
-                int blockNum = (row / 3) * 3 + col / 3;
-                if (rowUsed.getOrDefault(row, new HashSet<Character>()).contains(c) || colUsed.getOrDefault(col, new HashSet<Character>()).contains(c) || blockUsed.getOrDefault(blockNum, new HashSet<Character>()).contains(c)) continue;
-                board[row][col] = c;
-                rowUsed.computeIfAbsent(row, v -> new HashSet<Character>()).add(c);
-                colUsed.computeIfAbsent(col, v -> new HashSet<Character>()).add(c);
-                blockUsed.computeIfAbsent(blockNum, v -> new HashSet<Character>()).add(c);
-                if (col == 8) {
-                    if (dfs(board, row + 1, 0, rowUsed, colUsed, blockUsed)) return true;
-                } else {
-                    if (dfs(board, row, col + 1, rowUsed, colUsed, blockUsed)) return true;
+            return dfs(nr, nc, rowUsed, colUsed, boxUsed, board);
+        }
+        
+        for (int i = 1; i <= 9; i++) {
+            if (rowUsed[row][i] == 0 && colUsed[col][i] == 0 && boxUsed[box][i] == 0) {
+                rowUsed[row][i] = 1;
+                colUsed[col][i] = 1;
+                boxUsed[box][i] = 1;
+                board[row][col] = (char)('0' + i);
+                if (dfs(nr, nc, rowUsed, colUsed, boxUsed, board)) {
+                    return true;
                 }
+                rowUsed[row][i] = 0;
+                colUsed[col][i] = 0;
+                boxUsed[box][i] = 0;
                 board[row][col] = '.';
-                rowUsed.get(row).remove(c);
-                colUsed.get(col).remove(c);
-                blockUsed.get(blockNum).remove(c);
             }
         }
         return false;

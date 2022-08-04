fc2d8b8dc64b791bfc92dfb79329347617b522bc
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-04 16:16:18 +0200

    Time: 45 ms (78.20%), Space: 132.5 MB (36.24%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int n = temp.length;
+        int[] res = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temp[stack.peek()] <= temp[i]) stack.pop();
+            res[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+            stack.push(i);
+        }
+        return res;
+    }
+}

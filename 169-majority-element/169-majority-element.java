f71adf8a022483692b2f0b85dd08fc0ec57c35ba
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-05 20:47:09 +0200

    Time: 2 ms (89.27%), Space: 55.9 MB (60.39%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,21 +1,10 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        return findMajority(nums, 0, nums.length - 1);
-    }
-    
-    public int findMajority(int[] nums, int left, int right) {
-        if (left == right) return nums[right];
-        int mid = left + (right - left) / 2;
-        int leftMajor = findMajority(nums, left, mid);
-        int rightMajor = findMajority(nums, mid + 1, right);
-        if (leftMajor == rightMajor) return leftMajor;
-        int leftCount = 0, rightCount = 0;
-        for (int i = left; i <= right; i++) {
-            if (nums[i] == leftMajor) leftCount++;
-            if (nums[i] == rightMajor) rightCount++;
+        int candidate = -10000, count = 0;
+        for (int num : nums) {
+            if (count == 0) candidate = num;
+            count += candidate == num ? 1 : -1;
         }
-        return leftCount > rightCount ? leftMajor : rightMajor;
+        return candidate;
     }
 }
-
-//tc: O(nlogn) sc:O(log n)

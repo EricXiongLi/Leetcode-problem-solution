c9752cee384433f55ac9df94daf1cc6b2ee89609
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-06 18:09:32 +0200

    Time: 83 ms (20.06%), Space: 42.8 MB (34.06%) - LeetHub

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestSubstring(String s, int k) {
+        int res = Integer.MIN_VALUE;
+        for (int unique = 1; unique <= s.length() / k; unique++) {
+            int left = 0;
+            Map<Character, Integer> map = new HashMap<>();
+            int validCount = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                map.put(c, map.getOrDefault(c, 0) + 1);
+                if (map.get(c) == k) validCount++;
+                while (map.keySet().size() > unique) {
+                    char leftChar = s.charAt(left);
+                    if (map.get(leftChar) == k) validCount--;
+                    map.put(leftChar, map.get(leftChar) - 1);
+                    if (map.get(leftChar) == 0) map.remove(leftChar);
+                    left++;
+                }
+                int count = map.keySet().size();
+                if (count == unique && count == validCount) res = Math.max(res, i - left + 1);
+            }
+        }
+        return res == Integer.MIN_VALUE ? 0 : res;
+    }
+}

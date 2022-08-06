3de517ab8b1a2384f00384172835b08f3732abe0
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-06 19:29:53 +0200

    Time: 2 ms (75.79%), Space: 58.1 MB (28.31%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -3,13 +3,12 @@ class Solution {
         int res = Integer.MAX_VALUE, sum = 0, left = 0;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            if (sum >= target) {
+            
                 while (sum >= target) {
                     res = Math.min(res, i - left + 1);
                     sum -= nums[left];
                     left++;
                 }
-            }
         }
         return res == Integer.MAX_VALUE ? 0 : res;
     }

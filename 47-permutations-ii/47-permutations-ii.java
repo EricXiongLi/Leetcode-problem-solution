a524802ac2d8572931e4800723ac0e7cb4804da2
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-06 22:54:25 +0200

    Time: 2 ms (95.40%), Space: 43.3 MB (85.77%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,23 +1,27 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new LinkedList<>();
-        dfs(res, new ArrayDeque<Integer>(), nums, new boolean[nums.length]);
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> level = new ArrayDeque<>();
+        boolean[] used = new boolean[nums.length];
+        dfs(res, level, nums, used);
         return res;
     }
     
-    public void dfs(List<List<Integer>> res, Deque<Integer> tmp, int[] nums, boolean[] used) {
-        if (tmp.size() == nums.length) {
-            res.add(new ArrayList<>(tmp));
+    public void dfs(List<List<Integer>> res, Deque<Integer> level, int[] nums, boolean[] used) {
+        if (level.size() == nums.length) {
+            res.add(new ArrayList<>(level));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) continue;
-            tmp.offer(nums[i]);
+            //case1: used[i]
+            //case2: i - 1 >= 0 && nums[i] = nums[i - 1] && !used[i - 1]
+            if (used[i] || (i > 0 && nums[i] == nums[i -1] && !used[i - 1])) continue;
+            level.offer(nums[i]);
             used[i] = true;
-            dfs(res, tmp, nums, used);
-            tmp.pollLast();
+            dfs(res, level, nums, used);
             used[i] = false;
+            level.pollLast();
         }
     }
 }

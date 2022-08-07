29592c6de79e3daf366a076264305fa5a9ab4b2b
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-07 20:06:19 +0200

    Time: 46 ms (46.31%), Space: 67.5 MB (48.82%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -7,7 +7,7 @@ class Solution {
             sum += nums[i];
             if (map.containsKey(sum - k)) {
                 count += map.get(sum - k);
-            }
+            }//先验证，再放，要不然k = 0时会把自己也算上了
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;

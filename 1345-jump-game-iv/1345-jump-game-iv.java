591defca3efcb8ea6e96e4987083482b484c1600
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-10 19:46:21 +0200

    Time: 161 ms (42.64%), Space: 118.3 MB (55.30%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.java
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -1,47 +1,46 @@
 class Solution {
     public int minJumps(int[] nums) {
-        Queue<Integer> head = new LinkedList<>();
-        Queue<Integer> tail = new LinkedList<>();
-        Map<Integer, List<Integer>> map = new HashMap<>(); // <val, all the index with the val>
+        Set<Integer> head = new HashSet<>();
+        Set<Integer> tail = new HashSet<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.computeIfAbsent(nums[i], v -> new ArrayList<Integer>()).add(i);
         }
-        head.offer(0);
-        tail.offer(nums.length - 1);
+        head.add(0);
+        tail.add(nums.length - 1);
         boolean[] visited = new boolean[nums.length];
         int step = 0;
         while (!head.isEmpty() && !tail.isEmpty()) {
             if (head.size() > tail.size()) {
-                Queue<Integer> temp = head;
+                Set<Integer> temp = head;
                 head = tail;
                 tail = temp;
             }
-            int sz = head.size();
-            for (int i = 0; i < sz; i++) {
-                int cur = head.poll();
+            Set<Integer> next = new HashSet<>();
+            for (int cur : head) {
                 int left = cur - 1;
                 int right = cur + 1;
                 if (tail.contains(left) || tail.contains(right)) {
                     return step + 1;
                 }
                 if (left >= 0 && !visited[left]) {
-                    head.offer(left);
+                    next.add(left);
                     visited[left] = true;
                 }
                 if (right < nums.length && !visited[right]) {
-                    head.offer(right);
+                    next.add(right);
                     visited[right] = true;
                 }
-                for (int j : map.get(nums[cur])) {
-                    if (j != cur && tail.contains(j)) return step + 1;
-                    if (j != cur && !visited[j]) {
-                        head.offer(j);
-                        visited[j] = true;
+                for (int nei : map.get(nums[cur])) {
+                    if (nei != cur && tail.contains(nei)) return step + 1;
+                    if (nei != cur && !visited[nei])  {
+                        next.add(nei);
+                        visited[nei] = true;
                     }
                 }
-                map.get(nums[cur]).clear();
             }
             step++;
+            head = next;
         }
         return 0;
     }

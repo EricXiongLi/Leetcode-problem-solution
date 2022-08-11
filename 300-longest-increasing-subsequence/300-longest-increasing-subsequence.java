f218c2633a9c014726ddbec6d624356136b81fe7
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-11 16:34:02 +0200

    Time: 4 ms (94.51%), Space: 44.3 MB (62.72%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,28 +1,28 @@
 class Solution {
-  Integer[] dp;
     public int lengthOfLIS(int[] nums) {
-      //dp[i] : length of LIS when ending at i th;
-      //dp[i] = max(1, dp[j]) : j < i and nums[j] < nums[i]
-      dp = new Integer[nums.length];
-      dp[0] = 1;
-      int max = 1;
-      for (int i = 0; i < nums.length; i++) {
-        max = Math.max(dfs(nums, i), max);
-      }
-      return max;
-    }
-
-    private int dfs(int[] nums, int pos) {
-      if (dp[pos] != null) {
-        return dp[pos];
-      }
-      int max = 1;
-      for (int i = pos - 1; i >= 0; i--) {
-        if (nums[i] < nums[pos]) {
-          max = Math.max(max, 1 + dfs(nums, i));
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int size = 1;
+        for (int i = 1; i < n; i++) {
+            int insertPos = binarySearch(dp, size, nums[i]);
+            dp[insertPos] = nums[i];
+            if (insertPos == size) size++;
         }
-      }
-      dp[pos] = max;
-      return dp[pos];
+        return size;
+    }
+    
+    private int binarySearch(int[] nums, int size, int target) {
+        int left = 0, right = size - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+            
     }
 }

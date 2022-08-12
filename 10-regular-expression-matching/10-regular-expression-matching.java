249f4663d03521151b638e414ba12de06f130956
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-12 16:27:26 +0200

    Time: 2 ms (97.25%), Space: 41.1 MB (92.82%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -1,14 +1,34 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        if (p.isEmpty()) return s.isEmpty();
-        boolean firstMatch = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
-        if (p.length() >= 2 && p.charAt(1) == '*') {
-            boolean starMatchNull = isMatch(s, p.substring(2));
-            boolean starMatchStr = firstMatch && isMatch(s.substring(1), p);
-            return starMatchNull || starMatchStr;
-        } else {
-            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        //dp[i][j]: whether s.substring(i, end) and p.substring(j, end) martchs;
+        //
+        int l1 = s.length();
+        int l2 = p.length();
+        boolean[][] dp = new boolean[l1 + 1][l2 + 1];
+        dp[l1][l2] = true;
+        //
+        for (int j = l2 - 1; j >= 0; j--) {
+            if (p.charAt(j) == '*') continue;
+            if (j < l2 - 1 && p.charAt(j + 1) == '*') {
+                dp[l1][j] = dp[l1][j + 2];
+            } else {
+                dp[l1][j] = false;
+            }
         }
-        
+        for (int i = l1 - 1; i >= 0; i--) {
+            for (int j = l2 - 1; j >= 0; j--) {
+                boolean firstMatch = s.charAt(i) == p.charAt(j) || p.charAt(j) == '.';
+                if (j <= l2 - 2 && p.charAt(j + 1) == '*') {
+                    boolean starMatchNull = dp[i][j + 2];
+                    boolean starMatchStr = firstMatch && dp[i + 1][j];
+                    dp[i][j] = starMatchNull || starMatchStr;
+                } else {
+                    dp[i][j] = firstMatch && dp[i + 1][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+            
+            
     }
 }

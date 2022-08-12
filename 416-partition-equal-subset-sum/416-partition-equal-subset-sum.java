6d7effbe9d5653bc0fbb7ed332b3218e06c2380e
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-12 20:26:45 +0200

    Time: 86 ms (37.76%), Space: 54.6 MB (42.62%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        //dp[i][j]: the max sum when choosing between first i th  stones, with the available capacity of j
-        //dp[i][j] = max(dp[i][j - weight(i)] + val(i), dp[i - 1][j]);
         int sum = Arrays.stream(nums).sum();
         if (sum % 2 != 0) return false;
         sum /= 2;
-        int[][] dp = new int[nums.length][sum + 1];
-        for (int i = 1; i <= nums.length - 1; i++) {
+        boolean[][] dp = new boolean[nums.length + 1][sum + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i <= nums.length; i++) {
             for (int j = 1; j <= sum; j++) {
-                if (j >= nums[i]) {
-                    dp[i][j] = Math.max(dp[i - 1][j - nums[i]] + nums[i], dp[i - 1][j]);
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
                 } else {
                     dp[i][j] = dp[i - 1][j];
-                }               
+                }
             }
         }
-        return dp[nums.length - 1][sum] == sum;
+        return dp[nums.length][sum];
     }
 }

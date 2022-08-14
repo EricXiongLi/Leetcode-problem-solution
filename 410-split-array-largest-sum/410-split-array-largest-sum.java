faad7d9fe6d2b3d07666d311913bdb1403c7477c
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-14 16:16:18 +0200

    Time: 1 ms (87.18%), Space: 40 MB (91.30%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.java
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -1,33 +1,30 @@
 class Solution {
     public int splitArray(int[] nums, int m) {
-        int left = Integer.MAX_VALUE, right = 0;
-        for (int i : nums) {
-            right += i;
-            left = Math.min(left, i);
-        }
-        int res = right;
+        int sum = Arrays.stream(nums).sum();
+        int left = 0, right = sum;
+        int res = 0;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (canSplit(nums, m, mid)) {
-                right = mid - 1;
                 res = mid;
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return res;
+        return left;
     }
     
-    public boolean canSplit(int[] nums, int m, int mid) {
-        int curSum = 0, subarray = 1;
+    private boolean canSplit(int[] nums, int m, int mid) {
+        int sum = 0, subarrayNum = 1;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > mid) return false;
-            curSum += nums[i];
-            if (curSum > mid) {
-                subarray++;
-                curSum = nums[i];
+            sum += nums[i];
+            if (sum > mid) {
+                subarrayNum++;
+                sum = nums[i];
             }
         }
-        return subarray <= m;
+        return subarrayNum <= m;
     }
 }

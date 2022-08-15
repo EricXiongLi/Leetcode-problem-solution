3a8a22c9a480292170ff0679eee6484a0063b784
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-15 20:27:39 +0200

    Time: 5 ms (61.00%), Space: 43.8 MB (66.52%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int[] bitMap = new int[32];
+        for (int num : nums) {
+            for (int i = 0; i < 32; i++) {
+                bitMap[i] += 1 & (num >> i);
+            }
+        }
+        for (int i = 0; i < 32; i++) {
+            bitMap[i] %= 3;
+        }
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res |= (bitMap[i] << i);
+        }
+        return res;
+    }
+}

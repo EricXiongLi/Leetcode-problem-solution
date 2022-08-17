7f4c57f2fde2d9cdcbd811e57314a7bde4d6182a
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-17 20:51:52 +0200

    Time: 8 ms (69.24%), Space: 49.9 MB (39.11%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,31 +1,31 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        List<Integer> res = new LinkedList<Integer>();
         int[] indegree = new int[numCourses];
-        for (int[] pre : prerequisites) {
-            int end = pre[0], start = pre[1];
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int[] edge : prerequisites) {
+            int start = edge[1], end = edge[0];
             indegree[end]++;
-            graph.computeIfAbsent(start, v -> new ArrayList<>()).add(end);
+            graph.computeIfAbsent(start,  v-> new ArrayList<Integer>()).add(end);
         }
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < indegree.length; i++) {
+        for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
                 q.offer(i);
             }
         }
         int count = 0;
+        int[] res = new int[numCourses];
         while (!q.isEmpty()) {
             int cur = q.poll();
-            res.add(cur);
+            res[count] = cur;
             count++;
-            for (int i : graph.getOrDefault(cur, new ArrayList<>())) {
-                if (--indegree[i] == 0) {
-                    q.offer(i);
+            for (int nei : graph.getOrDefault(cur, new ArrayList<Integer>())) {
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    q.offer(nei);
                 }
             }
         }
-        
-        return count == numCourses ? res.stream().mapToInt(Integer::intValue).toArray() : new int[0];
+        return count == numCourses ? res : new int[0];
     }
 }

754f38ce6d362249f7907275190733889ed93313
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-18 19:20:19 +0200

    Time: 11 ms (40.27%), Space: 47 MB (53.90%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -1,26 +1,18 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, List<int[]>> graph = new HashMap<>();//[position, cost]
-        Map<Integer, Integer> visited = new HashMap<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);//[cost, position, stops left]
-        for (int[] edge : flights) {
-            graph.computeIfAbsent(edge[0], v-> new ArrayList<int[]>()).add(new int[]{edge[1], edge[2]});
-        }
-        pq.offer(new int[]{0, src, k + 1});
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int cost = cur[0], curPos = cur[1], stops = cur[2];
-            visited.put(curPos, stops);
-            if (curPos == dst) return cost;
-            if (stops > 0) {
-                for (int[] nei : graph.getOrDefault(curPos, new ArrayList<int[]>())) {
-                    int neiPos = nei[0], neiCost = nei[1];
-                    if (!visited.containsKey(neiPos) || stops > visited.get(neiPos)) {
-                        pq.offer(new int[]{cost + neiCost, neiPos, stops - 1});
-                    }
-                }
+        int dist[] = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i <= k; i++) {
+            int[] cur = Arrays.copyOf(dist, n);
+            for (int[] flight : flights) {
+                int start = flight[0], end = flight[1], cost = flight[2];
+                if (dist[start] == Integer.MAX_VALUE) continue;
+                cur[end] = Math.min(cur[end], dist[start] + cost);
             }
+            dist = cur;
         }
-        return -1;
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
     }
 }

d19311819950b0116f9e5512fa3290273ec061b4
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-19 12:03:39 +0200

    Time: 12 ms (8.46%), Space: 53.5 MB (70.50%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -7,23 +7,29 @@ class Solution {
         color = new int[n];
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                if (!dfs(graph, i, 1)) return false;
+                if (!bfs(graph, i)) return false;
             }
         }
         return true;
     }
     
-    private boolean dfs(int[][] graph, int cur, int preColor) {
-        visited[cur] = true;
-        color[cur] = preColor * -1;
-        for (int nei : graph[cur]) {
-            if (!visited[nei]) {
-                if (!dfs(graph, nei, color[cur])) return false;
-            } else {
-                if (color[nei] == color[cur]) return false;
+    private boolean bfs(int[][] graph, int root) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(root);
+        color[root] = 1;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            visited[cur] = true;
+            
+            for (int nei : graph[cur]) {
+                if (!visited[nei]) {
+                    color[nei] = -1 * color[cur];
+                    q.offer(nei);
+                } else {
+                    if (color[nei] == color[cur]) return false;
+                }
             }
         }
         return true;
-        
     }
 }

8fac9af02eba9aeb84a187bd5ef71864d0785fee
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-19 22:40:49 +0200

    Time: 126 ms (9.23%), Space: 76.9 MB (7.64%) - LeetHub

---
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumCost(int n, int[][] connections) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] edge: connections) {
+            int start = edge[0], end = edge[1], cost = edge[2];
+            graph.computeIfAbsent(start, v -> new ArrayList<int[]>()).add(new int[]{end, cost});
+            graph.computeIfAbsent(end, v -> new ArrayList<int[]>()).add(new int[]{start, cost});
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        pq.offer(new int[]{1, 1, 0});
+        Set<Integer> visited = new HashSet<>();
+        int costs = 0;
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int curStart = cur[0], curEnd = cur[1], curCost = cur[2];
+            if (visited.contains(curEnd)) continue;
+            costs += curCost;
+            visited.add(curEnd);
+            for (int[] nei : graph.getOrDefault(curEnd, new ArrayList<int[]>())) {
+                int neiEnd = nei[0], neiCost = nei[1];
+                if (!visited.contains(neiEnd)) {
+                    pq.offer(new int[]{curEnd, neiEnd, neiCost});
+                }
+            }
+        }
+        return visited.size() == n ? costs : -1;
+    }
+}

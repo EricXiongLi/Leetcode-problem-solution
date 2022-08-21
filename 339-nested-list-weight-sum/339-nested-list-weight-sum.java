ba9ecedd584ad4bd68a0c4d88b16171a9275ee30
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-21 11:06:11 +0200

    Time: 3 ms (18.46%), Space: 41.7 MB (42.95%) - LeetHub

---
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.java
@@ -0,0 +1,52 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        Queue<NestedInteger> q = new LinkedList<>();
+        int depth = 1;
+        int sum = 0;
+        for (NestedInteger ni : nestedList) {
+            q.offer(ni);
+        }
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                NestedInteger cur = q.poll();
+                if (cur.isInteger()) {
+                    sum += cur.getInteger() * depth;
+                }
+                for (NestedInteger nei : cur.getList()) {
+                    q.offer(nei);
+                }
+            }
+            depth++;
+        }
+        return sum;
+    }
+}

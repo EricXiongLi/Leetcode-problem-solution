bf383c3decec82bcbbbf6668fdd2380057a597f1
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-08-22 20:20:39 +0200

    Time: 154 ms (24.49%), Space: 99.4 MB (42.29%) - LeetHub

--- 815-bus-routes/815-bus-routes.java
+++ 815-bus-routes/815-bus-routes.java
@@ -20,21 +20,32 @@ class Solution {
                 }
             }
         }
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
         Set<Integer> visited = new HashSet<>();
         for (int i : sourceSet) {
-            q.offer(i);
+            if (targetSet.contains(i)) return 1;
+            q1.offer(i);
             visited.add(i);
         }
+        for (int j : targetSet) {
+            q2.offer(j);
+            visited.add(j);
+        }
         int step = 1;
-        while (!q.isEmpty()) {
-            int sz = q.size();
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            if (q1.size() > q2.size()) {
+                Queue<Integer> tmp = q1;
+                q1 = q2;
+                q2 = tmp;
+            }
+            int sz = q1.size();
             for (int i = 0; i < sz; i++) {
-                int cur = q.poll();
-                if (targetSet.contains(cur)) return step;
+                int cur = q1.poll();
                 for (int nei : graph.getOrDefault(cur, new HashSet<Integer>())) {
+                    if (q2.contains(nei)) return step + 1;
                     if (visited.contains(nei)) continue;
-                    q.offer(nei);
+                    q1.offer(nei);
                     visited.add(nei);
                 }
             }

6dadb49953b1954c3de1f4ca793607035ac13137
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-09-02 18:53:17 +0200

    Time: 3 ms (86.56%), Space: 53.7 MB (83.82%) - LeetHub

--- 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.java
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.java
@@ -1,14 +1,22 @@
 class Solution {
-    
     public int numSubarraysWithSum(int[] nums, int goal) {
-        Map<Integer, Integer> map = new HashMap<>();//<prefix sum, frequency>
-        map.put(0, 1);
-        int res = 0;
+        int l1 = 0, l2 = 0;
+        int n = nums.length;
         int sum = 0;
-        for (int num : nums) {
-            sum += num;
-            res += map.getOrDefault(sum - goal, 0);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        int res = 0;
+        int s1 = 0, s2 = 0;
+        for (int r = 0; r < n; r++) {
+            s1 += nums[r];
+            s2 += nums[r];
+            while (s1 > goal && l1 <= r) {
+                s1 -= nums[l1];
+                l1++;
+            }
+            while (s2 >= goal && l2 <= r) {
+                s2 -= nums[l2];
+                l2++;
+            }
+            res += (l2 - l1);
         }
         return res;
     }

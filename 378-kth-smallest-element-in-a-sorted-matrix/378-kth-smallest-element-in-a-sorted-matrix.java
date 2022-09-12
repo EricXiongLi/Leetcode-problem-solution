91c91e252aa68069e0b2b68d561d5f29bc117ff0
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-09-12 11:46:22 +0200

    Time: 1 ms (89.43%), Space: 56.3 MB (73.66%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,13 +1,21 @@
 class Solution {
+    int n;
+    
     public int kthSmallest(int[][] matrix, int k) {
-        // 1,  5,  9
-        // 10, 11, 13
-        // 12, 13, 15
-        int n = matrix.length;
+//         1,  5,  9
+//         10, 11, 13
+//         12, 13, 15
+//         mid: 11
+//         1, 5, 9, 10, 11, 12, 14, 14, 15
+            
+//         1, 5, 9, 10, 11, 12, 14, 15
+// <=cur   1，2， 3，4， 5，  6， 8， 9
+// < k     t  t  t  t. t.   t.  f f
+        
+        n = matrix.length;
         int l = matrix[0][0], r = matrix[n - 1][n - 1];
-        int res;
-        //true, true,..., true, false, false
         while (l <= r) {
+            //log(r - l)
             int mid = l + (r - l) / 2;
             if (check(mid, matrix, k)) {
                 l = mid + 1;
@@ -18,16 +26,14 @@ class Solution {
         return l;
     }
     
-    public boolean check(int mid, int[][] matrix, int k) {
-        int n = matrix.length;
+    private boolean check(int mid, int[][] matrix, int k) {
         int startRow = n - 1, startCol = 0;
         int count = 0;
-        boolean isExsit = false;
         while (startRow >= 0 && startCol <= n - 1) {
             if (matrix[startRow][startCol] <= mid) {
-                if (matrix[startRow][startCol] == mid) isExsit = true;
                 count += (startRow + 1);
                 startCol++;
+                
             } else {
                 startRow--;
             }
@@ -35,3 +41,6 @@ class Solution {
         return count < k;
     }
 }
+
+//tc: O(nlog(r - l))
+//sc: O(1)

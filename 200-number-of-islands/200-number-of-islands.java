5f2f75dc9b961fca27298e9978940df7ddb5ddf2
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-09-14 22:59:07 +0200

    Time: 14 ms (12.39%), Space: 57.1 MB (66.81%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,29 +1,64 @@
 class Solution {
-    int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    int count = 0;
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int m = grid.length, n = grid[0].length;
+        int[][] dirs = new int[][]{{1, 0}, {0, 1}};
+        DSU dsu = new DSU(m * n);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
+                    for (int[] dir : dirs) {
+                        int newRow = i + dir[0];
+                        int newCol = j + dir[1];
+                        if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && grid[newRow][newCol] == '1') {
+                            dsu.union(i * n + j, newRow * n + newCol);
+                        }
+                    }
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dsu.parent[i * n + j] == i * n + j && grid[i][j] == '1') {
                     count++;
-                    dfs(grid, i, j, m, n);
                 }
             }
         }
         return count;
     }
+
     
-    private void dfs(char[][] grid, int row, int col, int m, int n) {
-        grid[row][col] = '2';
-        for (int[] dir : dirs) {
-            int newRow = row + dir[0];
-            int newCol = col + dir[1];
-            if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && grid[newRow][newCol] == '1') {
-                dfs(grid, newRow, newCol, m, n);
-            }
+}
+
+class DSU {
+    int[] parent;
+    int[] size;
+    
+    public DSU(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
-        
+        Arrays.fill(size, 1);
+    }
+    
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    
+    public void union(int x, int y) {
+        int rootX = find(x), rootY = find(y);
+        if (rootX == rootY) return;
+        if (size[rootX] < size[rootY]) {
+            parent[rootX] = rootY;
+            size[rootY] += size[rootX];
+        } else {
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
+        }
     }
 }

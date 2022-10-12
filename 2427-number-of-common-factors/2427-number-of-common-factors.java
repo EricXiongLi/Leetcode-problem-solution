65c6ba7b35d88ce8c97f4eeaab1edc9c6ef4a596
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-10-12 15:11:40 +0200

    Time: 1 ms (80.67%), Space: 40 MB (83.37%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int count = 0;
+        for (int i = 1; i <= gcd(a, b); i++) {
+            if (a % i == 0 && b % i == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    int gcd(int a, int b) {
+        if (a == 0) return b;
+        return gcd(b % a, a);
+        
+    }
+}

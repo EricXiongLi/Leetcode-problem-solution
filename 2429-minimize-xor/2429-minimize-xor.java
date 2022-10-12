7a53a1c7b6ae6cbf65f5f0fa8f5041b81083397f
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-10-12 16:52:16 +0200

    Time: 1 ms (93.19%), Space: 40.8 MB (75.60%) - LeetHub

--- 2429-minimize-xor/2429-minimize-xor.java
+++ 2429-minimize-xor/2429-minimize-xor.java
@@ -1,21 +1,18 @@
 class Solution {
     public int minimizeXor(int num1, int num2) {
-        int[] bit1 = new int[32], bit2 = new int[32];
-        int[] freq = new int[2];
-        for (int i = 0; i < 32; i++) {
-            bit1[i] = (num1 >> i) & 1;
-            bit2[i] = (num2 >> i) & 1;
-            freq[bit2[i]]++;
-        }
+        int[] count = new int[2];
+        count[1] = Integer.bitCount(num2);
+        count[0] = 32 - count[1];
         int res = 0;
         for (int i = 31; i >= 0; i--) {
-            if (freq[bit1[i]] > 0) {
-                if (bit1[i] == 1) res += (1 << i);
-                freq[bit1[i]]--;
-            } else {
-                int val = 1 - bit1[i];
+            int val = (num1 >> i) & 1;
+            if (count[val] > 0) {
+                count[val]--;
                 if (val == 1) res += (1 << i);
-                freq[val]--;
+            } else {
+                int val2 = val ^ 1;
+                count[val2]--;
+                if (val2 == 1) res += (1 << i);
             }
         }
         return res;

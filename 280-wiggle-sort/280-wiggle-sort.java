02ed697bb1ead1095b3522d06947604b38dda328
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-10-19 22:12:55 +0200

    Time: 1 ms (92.49%), Space: 49.6 MB (22.16%) - LeetHub

---
+++ 280-wiggle-sort/280-wiggle-sort.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (i % 2 == 0 && nums[i] > nums[i - 1]) {
+                swap(nums, i, i - 1);
+            }
+            if (i % 2 == 1 && nums[i] < nums[i - 1]) {
+                swap(nums, i, i - 1);
+            }
+        }
+    }
+    
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}

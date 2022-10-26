20e298f76a579b8d3a7acae41090e050946f8d3a
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-10-26 21:04:04 +0200

    Time: 18 ms (55.25%), Space: 44.7 MB (62.79%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
@@ -1,20 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        Map<Character, Integer> map = new HashMap<>();//< char, frequency>
-        int left = 0;
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char r = s.charAt(i);
-            map.put(r, map.getOrDefault(r, 0) + 1);
+        Map<Character, Integer> map = new HashMap<>();
+        int l = 0;
+        int res = 1;
+        if (k == 0) return 0;
+        for (int r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+            map.put(c, map.getOrDefault(c, 0) + 1);
             while (map.size() > k) {
-                char l = s.charAt(left);
-                map.put(l, map.get(l) - 1);
-                if (map.get(l) == 0) {
-                    map.remove(l);
+                char leftC = s.charAt(l);
+                map.put(leftC, map.get(leftC) - 1);
+                if (map.get(leftC) == 0) {
+                    map.remove(leftC);
                 }
-                left++;
+                l++;
             }
-            res = Math.max(res, i - left + 1);
+            res = Math.max(r - l + 1, res);
         }
         return res;
     }

4c27f4687496eff3d51d886ee3a72b1a426e4370
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-10-27 15:39:17 +0200

    Time: 12 ms (87.67%), Space: 43.9 MB (6.81%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.java
+++ 259-3sum-smaller/259-3sum-smaller.java
@@ -1,24 +1,17 @@
 class Solution {
     public int threeSumSmaller(int[] nums, int target) {
         Arrays.sort(nums);
+        int n = nums.length;
         int count = 0;
-        //-2, 0, 1, 6
-        for (int i = 0; i < nums.length - 2; i++) {
-            count += twoSumSmaller(i + 1, nums, target - nums[i]);
-        }
-        return count;
-    }
-    
-    //1, 2, 3, 4
-    private int twoSumSmaller(int i, int[] nums, int target) {
-        int count = 0;
-        int l = i, r = nums.length - 1;
-        while (l < r) {
-            if (nums[l] + nums[r] < target) {
-                count += (r - l);
-                l++;
-            } else {
-                r--;
+        for (int i = n - 1; i >= 2; i--) {
+            int l = 0, r = i - 1;
+            while (l < r) {
+                if (nums[i] + nums[l] + nums[r] < target) {
+                    count += (r - l);
+                    l++;
+                } else {
+                    r--;
+                }
             }
         }
         return count;

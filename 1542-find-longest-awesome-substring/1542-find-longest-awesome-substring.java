374370bcde9740072347cb479572577302900eac
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-10-30 21:49:35 +0100

    Time: 334 ms (5.66%), Space: 117.6 MB (24.53%) - LeetHub

--- 1542-find-longest-awesome-substring/1542-find-longest-awesome-substring.java
+++ 1542-find-longest-awesome-substring/1542-find-longest-awesome-substring.java
@@ -1,27 +1,24 @@
 class Solution {
     public int longestAwesome(String s) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int cur=0;  //状态
-        int ans=1;  //记录答案
-        map.put(cur,-1); 
-        for(int c=0;c<s.length();c++){
-            int ch=s.charAt(c)-'0';
-            //计数
-            cur=cur^(1<<ch);
-            //一个数字出现奇数次，其余出现偶数次
-            for(int i=0;i<10;i++){
-                int next=cur^(1<<i);
-                if(map.containsKey(next)){
-                    ans=Math.max(ans,c-map.get(next));
+        int count = 0;
+        int res = 1;
+        Map<Integer, Integer> map = new HashMap<>(); //<freq : index for first occurence>
+        map.put(0, -1);
+        for (int i = 0; i < s.length(); i++) {
+            int val = s.charAt(i) - '0';
+            count ^= (1 << val);
+            if (map.containsKey(count)) {
+                res = Math.max(res, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+            for (int j = 0; j <= 9; j++) {
+                int cur = count ^ (1 << j);
+                if (map.containsKey(cur)) {
+                    res = Math.max(res, i - map.get(cur));
                 }
             }
-            //所有都出现了偶数次
-            if(!map.containsKey(cur)){
-                map.put(cur,c);
-            }else{
-                ans=Math.max(ans,c-map.get(cur));
-            }
         }
-        return ans;
+        return res;
     }
 }

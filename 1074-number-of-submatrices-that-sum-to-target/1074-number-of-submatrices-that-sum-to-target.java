1821c475664efc25b176beb1b56cd07b3ed9ad78
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-11-02 22:04:25 +0100

    Time: 327 ms (43.62%), Space: 115.7 MB (73.59%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,25 +1,25 @@
 class Solution {
-    public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int m = matrix.length, n = matrix[0].length;
+    public int numSubmatrixSumTarget(int[][] nums, int target) {
+        int m = nums.length, n = nums[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                matrix[i][j] = matrix[i][j - 1] + matrix[i][j];
+                nums[i][j] = nums[i][j - 1] + nums[i][j];
             }
         }
-        int res = 0;
         Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 map.clear();
                 map.put(0, 1);
                 int sum = 0;
                 for (int k = 0; k < m; k++) {
-                    sum += matrix[k][j] - (i == 0 ? 0 : matrix[k][i - 1]);
-                    res += map.getOrDefault(sum - target, 0);
+                    sum += nums[k][j] - (i == 0 ? 0 : nums[k][i - 1]);
+                    count += map.getOrDefault(sum - target, 0);
                     map.put(sum, map.getOrDefault(sum, 0) + 1);
                 }
             }
         }
-        return res;
+        return count;
     }
 }

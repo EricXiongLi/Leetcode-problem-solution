3fa67245a41b6860a97db13a12d6207e1771179a
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-11-06 06:56:00 +0100

    Time: 123 ms (80.00%), Space: 58.7 MB (100.00%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
@@ -2,13 +2,13 @@ class Solution {
     public long totalCost(int[] costs, int k, int c) {
         PriorityQueue<int[]> pq1 = new PriorityQueue<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
         PriorityQueue<int[]> pq2 = new PriorityQueue<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
-        Set<Integer> deleted = new HashSet<>();
+        
         int n = costs.length;
         int l = c - 1;
         for (int i = 0; i < c; i++) {
             pq1.offer(new int[]{costs[i], i});
         }
-        System.out.println("n:" + n);
+        
         long res = 0;
         int r = n - 1;
         for (r = n - 1; r > l && r >= n - c ; r--) {
@@ -25,21 +25,21 @@ class Solution {
                     pq2.offer(new int[]{costs[r - 1], r - 1});
                     r--;
                 }
-                deleted.add(min2[1]);
+                
                 k--;
-                System.out.println("am");
+                
             } else if (pq2.isEmpty()) {
                 int[] min1 = pq1.peek();
                  res += min1[0];
-                System.out.println(":" + min1[0]);
+                
                 pq1.poll();
                 if (l < r - 1) {
                     pq1.offer(new int[]{costs[l + 1], l + 1});
                     l++;
                 }
-                deleted.add(min1[1]);
+                
                 k--;
-                System.out.println("am");
+                
             }
             else {
                 // System.out.println("pq1:");
@@ -62,8 +62,7 @@ class Solution {
                         l++;
                     }
                    
-                    deleted.add(min1[1]);
-                    System.out.println("added" + min1[0]);
+                    
                     k--;
                 } else {
                 res += min2[0];
@@ -75,8 +74,7 @@ class Solution {
                 }
            
                 
-                deleted.add(min2[1]);
-                System.out.println("added" + min2[0]);
+                
                 k--;
             }
             }

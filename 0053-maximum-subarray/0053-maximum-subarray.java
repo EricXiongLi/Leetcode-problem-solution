070cdf1480c640a626bae0026ae633009579f233
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-11-08 08:14:13 +0100

    Time: 2 ms (79.41%), Space: 74 MB (39.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,12 +4,13 @@ class Solution {
         int n = nums.length;
         int res = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
-            if (prev >= 0) {
-                prev += nums[i];
-            } else {
+            if (prev <= 0) {
+                res = Math.max(nums[i], res);
                 prev = nums[i];
+            } else {
+                prev += nums[i];
+                res = Math.max(prev, res);
             }
-            res = Math.max(prev, res);
         }
         return res;
     }

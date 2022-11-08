25b14ab6a8d89cba7f8db89e1c9b351e1f5f5046
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-11-08 07:31:42 +0100

    Time: 2 ms (98.79%), Space: 42.4 MB (75.61%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -5,7 +5,7 @@ class Solution {
         if (n ==0) return "";
         sb.append(s.charAt(0));
         for (int i = 1; i < n; i++) {
-            if (sb.length() > 0 && (s.charAt(i) ^ 32) == sb.charAt(sb.length() - 1)) {
+            if (sb.length() > 0 && (s.charAt(i) ^ ' ') == sb.charAt(sb.length() - 1)) {
                 sb.deleteCharAt(sb.length() - 1);
             } else {
                 sb.append(s.charAt(i));

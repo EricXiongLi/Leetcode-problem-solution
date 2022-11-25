8f054d76a6ea6f32d26c94f854b73bf7a5b08e39
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2022-11-25 17:49:53 +0100

    Time: 40 ms (52.48%), Space: 59.3 MB (46.79%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        //[-4, -1, -1, 0, 1, 2]
         List<List<Integer>> res = new ArrayList<>();
         int n = nums.length;
-        for (int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < n - 2; i++)  {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int target = -nums[i];
             int l = i + 1, r = n - 1;
-            if (i != 0 && nums[i] == nums[i - 1]) continue;
             while (l < r) {
+                int curSum = nums[l] + nums[r];
                 if (l != i + 1 && nums[l] == nums[l - 1]) {
                     l++;
                     continue;
                 }
-                if (r != n - 1 && nums[r] == nums[r + 1]) {
-                    r--;
-                    continue;
-                }
-                if (nums[l] + nums[r] + nums[i] == 0) {
+                
+                if (nums[l] + nums[r] == target) {
+                    List<Integer> level = new ArrayList<>();
                     res.add(Arrays.asList(nums[i], nums[l], nums[r]));
                     l++;
                     r--;
-                } else if (nums[l] + nums[r] + nums[i] > 0) {
-                    r--;
-                } else {
+                } else if (curSum < target) {
                     l++;
+                } else {
+                    r--;
                 }
             }
         }

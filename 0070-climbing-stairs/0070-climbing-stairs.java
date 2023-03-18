823bedbb7887e6528bcf9991718d4673434fc73d
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2023-03-18 18:01:13 +0100

    Time: 0 ms (100.00%), Space: 39.5 MB (24.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,11 +4,14 @@ class Solution {
         //dp[i] = dp[i - 1] + dp[i - 2]
         if (n == 1) return 1;
         int[] dp = new int[n];
-        dp[0] = 1;
-        dp[1] = 2;
+        int a = 1;
+        int b = 2;
+        int c = 0;
         for (int i = 2; i < n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            c = a + b;
+            a = b;
+            b = c;
         }
-        return dp[n - 1];
+        return b;
     }
 }

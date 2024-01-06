4e1fb73a8aa7ef6cd1cecc8dd83bec0bb7b5f7e4
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2024-01-06 20:21:23 +0100

    Time: 1 ms (100.00%), Space: 45.4 MB (25.17%) - LeetHub

--- 0490-the-maze/0490-the-maze.java
+++ 0490-the-maze/0490-the-maze.java
@@ -1,38 +1,31 @@
 class Solution {
-    int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
         int m = maze.length, n = maze[0].length;
-        
         boolean[][] visited = new boolean[m][n];
-        int startX = start[0], startY = start[1];
-        visited[startX][startY] = true;
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(start);
-        return bfs(maze, start, destination, q, m, n, visited);
+        return dfs(maze, start[0], start[1], destination, m, n, visited);
     }
     
-    public boolean bfs(int[][] maze, int[] start, int[] destination, Queue<int[]> q, int m, int n, boolean[][] visited) {
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int curX = cur[0], curY = cur[1];
-            if (curX == destination[0] && curY == destination[1]) {
-                    return true;
+    public boolean dfs(int[][] maze, int startX, int startY, int[] destination, int m, int n, boolean[][] visited) {
+        if (visited[startX][startY]) {
+            return false;
+        }
+        if (startX == destination[0] && startY == destination[1]) {
+            return true;
+        }
+        visited[startX][startY] = true;
+        for (int[] dir : dirs) {
+            int x = startX,  y = startY;
+            while (x + dir[0] >= 0 && x + dir[0] < m && y + dir[1] >= 0 && y + dir[1] < n && maze[x + dir[0]][y + dir[1]] == 0) {
+                x += dir[0];
+                y += dir[1];
             }
-            for (int[] dir : dirs) {
-                int x = cur[0], y = cur[1];
-                while(x + dir[0] >= 0 && x + dir[0] < m && y + dir[1] >= 0 && y + dir[1] < n && maze[x + dir[0]][y + dir[1]] == 0) {
-                    x += dir[0];
-                    y += dir[1];
-                }
-                
-                
-                if (!visited[x][y]) {
-                    q.offer(new int[]{x, y});
-                    visited[x][y] = true;
-                }
+            if (dfs(maze, x, y, destination, m, n, visited)) {
+                return true;
             }
         }
+            
         return false;
     }
 }

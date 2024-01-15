5821f81fa8a8d2231a88260ce18f3225d2f48bfb
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2024-01-15 09:38:46 +0100

    Time: 2 ms (43.32%), Space: 44.8 MB (38.91%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,31 +1,56 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        int[] visited = new int[n];
+        DSU dsu = new DSU(n);
+        
         for (int i = 0; i < n; i++) {
-            if (visited[i] == 0) {
-                visited[i] = 1;
-                if (!dfs(graph, i, visited)) {
+            for (int nei : graph[i]) {
+                if (dsu.isConnected(i, nei)) {
                     return false;
                 }
+                dsu.union(nei, graph[i][0]);
             }
         }
-        
         return true;
     }
     
-    public boolean dfs(int[][] graph, int cur, int[] visited) {
-        for (int nei : graph[cur]) {
-            if (visited[nei] == visited[cur]) {
-                return false;
-            }
-            if (visited[nei] == 0) {
-                visited[nei] = -1 * visited[cur];
-                if (!dfs(graph, nei, visited)) {
-                    return false;
-                }
-            }
+}
+
+public class DSU {
+    int[] parent;
+    int[] size;
+    
+    public DSU(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
-        return true;
+        Arrays.fill(size, 1);
+    }
+    
+    public int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        
+        if (rootX < rootY) {
+            parent[rootX] = rootY;
+            size[rootY] += size[rootX];
+        } else {
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
+        }
+    }
+    
+    public boolean isConnected(int x, int y) {
+        return find(x) == find(y);
     }
 }

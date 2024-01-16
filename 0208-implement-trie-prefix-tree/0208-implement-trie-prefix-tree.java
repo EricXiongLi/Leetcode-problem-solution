69d4478243e7cdd9f06ac0f11e633cb75883d42e
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2024-01-16 22:24:42 +0100

    Time: 32 ms (94.92%), Space: 55.2 MB (53.02%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,5 +1,6 @@
 class Trie {
     TrieNode root;
+    
     public Trie() {
         root = new TrieNode();
     }
@@ -7,7 +8,9 @@ class Trie {
     public void insert(String word) {
         TrieNode node = root;
         for (char c : word.toCharArray()) {
-            if (node.children[c - 'a'] == null) node.children[c - 'a'] = new TrieNode();
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
             node = node.children[c - 'a'];
         }
         node.isWord = true;
@@ -16,22 +19,31 @@ class Trie {
     public boolean search(String word) {
         TrieNode node = root;
         for (char c : word.toCharArray()) {
-            if (node.children[c - 'a'] == null) return false;
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
             node = node.children[c - 'a'];
         }
+        
         return node.isWord;
     }
     
     public boolean startsWith(String prefix) {
         TrieNode node = root;
         for (char c : prefix.toCharArray()) {
-            if (node.children[c - 'a'] == null) return false;
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
             node = node.children[c - 'a'];
         }
         return true;
     }
 }
 
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isWord;
+}
 /**
  * Your Trie object will be instantiated and called as such:
  * Trie obj = new Trie();
@@ -39,11 +51,3 @@ class Trie {
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
-
-class TrieNode {
-    TrieNode[] children;
-    boolean isWord;
-    public TrieNode() {
-        children = new TrieNode[26];
-    }
-}

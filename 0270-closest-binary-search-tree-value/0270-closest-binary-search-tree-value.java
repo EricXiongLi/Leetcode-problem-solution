d95606d0bb7932a64ed5f5961ee1caf303fe4eea
XiongLI <91139789+EricXiongLi@users.noreply.github.com>
2024-02-22 00:18:00 +0100

    Time: 0 ms (100.00%), Space: 42.7 MB (85.44%) - LeetHub

---
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int closestValue(TreeNode root, double target) {
+        double diff = Double.MAX_VALUE;
+        int res = 0;
+        TreeNode node = root;
+        while (node != null) {
+            double curDiff = Math.abs(target - node.val);
+            if (curDiff == diff) {
+                res = Math.min(node.val, res);
+            } else if (curDiff < diff) {
+                res = node.val;
+                diff = curDiff;
+            }
+            if (node.val == target) {
+                return node.val;
+            } else if (node.val > target) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        
+        return res;
+    }
+}

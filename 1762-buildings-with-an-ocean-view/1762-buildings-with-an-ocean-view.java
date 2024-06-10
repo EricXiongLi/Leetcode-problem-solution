61b3e6b2493b6e9cf20b474f33f4a6c463f967a9
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-06-10 09:47:15 +0200

    Time: 50 ms (13.09%), Space: 63.2 MB (5.10%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
@@ -1,15 +1,32 @@
 class Solution {
     public int[] findBuildings(int[] heights) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        Deque<Integer> res = new ArrayDeque<>();
         int n = heights.length;
-        for (int i = n - 1; i >= 0; i--) {
-            if (stack.isEmpty() || heights[i] > stack.peek()) {
-                stack.push(heights[i]);
-                res.push(i);
+        List<Integer> list = new ArrayList<>();
+        
+        // Monotonically decreasing stack.
+        Stack<Integer> stack = new Stack<>();  
+        for (int current = n - 1; current >= 0; --current) {
+            // If the building to the right is smaller, we can pop it.
+            while (!stack.isEmpty() && heights[stack.peek()] < heights[current]) {
+                stack.pop();
             }
+            
+            // If the stack is empty, it means there is no building to the right 
+            // that can block the view of the current building.
+            if (stack.isEmpty()) { 
+                list.add(current);
+            }
+            
+            // Push the current building in the stack.
+            stack.push(current);
         }
-        int[] res2 = res.stream().mapToInt(Integer::intValue).toArray();
-        return res2;
+ 
+        // Push elements from list to answer array in reverse order.
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); ++i) {
+            answer[i] = list.get(list.size() - 1 - i);
+        }
+        
+        return answer;
     }
 }

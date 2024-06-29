5fb2cea7a7fbc76d32508196d32b3f1354943ac2
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-06-29 10:24:49 +0100

    Time: 28 ms (76.74%), Space: 101.7 MB (56.64%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.java
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.java
@@ -1,20 +1,30 @@
 class Solution {
     public List<List<Integer>> findRLEArray(int[][] encoded1, int[][] encoded2) {
         List<List<Integer>> res = new ArrayList<>();
+        
         int p1 = 0, p2 = 0;
-        int n1 = encoded1.length, n2  = encoded2.length;
-        while (p1 < n1 && p2 < n2) {
-            int minFreq = Math.min(encoded1[p1][1], encoded2[p2][1]);
-            int curProd = encoded1[p1][0] * encoded2[p2][0];
-            encoded1[p1][1] -= minFreq;
-            if (encoded1[p1][1] == 0) p1++;
-            encoded2[p2][1] -= minFreq;
-            if (encoded2[p2][1] == 0) p2++;
-            if (res.isEmpty() || res.get(res.size() - 1).get(0) != curProd) {
-                res.add(Arrays.asList(curProd, minFreq));
+        
+        while (p1 < encoded1.length) {
+            int minL = Math.min(encoded1[p1][1], encoded2[p2][1]);
+            
+            int prod = encoded1[p1][0] * encoded2[p2][0];
+            int sz = res.size();;
+            if (sz > 0 && res.get(sz - 1).get(0) == prod) {
+                res.get(sz - 1).set(1, res.get(sz - 1).get(1) + minL);
             } else {
-                res.get(res.size() - 1).set(1, res.get(res.size() - 1).get(1) + minFreq);
+                res.add(Arrays.asList(prod, minL));
             }
+            encoded1[p1][1] -= minL;
+            encoded2[p2][1] -= minL;
+            
+            if (encoded1[p1][1] == 0) {
+                p1++;
+            }
+            if (encoded2[p2][1] == 0) {
+                p2++;
+            }
+            
+            
         }
         return res;
     }

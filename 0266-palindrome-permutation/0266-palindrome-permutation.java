a09f98d99010f86d03060db27f00ace6976799e3
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-07-07 08:59:05 +0200

    Time: 1 ms (48.76%), Space: 41.3 MB (43.17%) - LeetHub

---
+++ 0266-palindrome-permutation/0266-palindrome-permutation.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        
+        for (char c : s.toCharArray()) {
+            if (set.contains(c)) {
+                set.remove(c);
+            } else {
+                set.add(c);
+            }
+        }
+        
+        return set.size() <= 1;
+    }
+}

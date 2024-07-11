c9b62efd6aa6be178c75d698d77b0c38a5f1e475
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-07-11 14:32:21 +0200

    Time: 98 ms (19.96%), Space: 56.8 MB (6.34%) - LeetHub

---
+++ 0911-online-election/0911-online-election.java
@@ -0,0 +1,28 @@
+class TopVotedCandidate {
+    //person -> times
+    Map<Integer, Integer> count = new HashMap();
+    
+    //time -> leading person
+    TreeMap<Integer, Integer> map = new TreeMap();
+    
+    public TopVotedCandidate(int[] persons, int[] times) {
+        int lead = -1;
+        for (int i = 0; i < times.length; i++) {
+            count.put(persons[i], count.getOrDefault(persons[i], 0) + 1);
+            if (count.get(persons[i]) >= (lead == -1 ? 0 : count.get(lead))) {
+                lead = persons[i];
+            }
+            map.put(times[i], lead);
+        }
+    }
+    
+    public int q(int t) {
+        return map.floorEntry(t).getValue();
+    }
+}
+
+/**
+ * Your TopVotedCandidate object will be instantiated and called as such:
+ * TopVotedCandidate obj = new TopVotedCandidate(persons, times);
+ * int param_1 = obj.q(t);
+ */

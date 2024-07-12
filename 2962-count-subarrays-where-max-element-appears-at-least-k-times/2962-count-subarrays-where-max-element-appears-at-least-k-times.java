14d6223917465ffe6381e6fe5ac56333c437609f
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-07-12 11:21:30 +0200

    Time: 101 ms (5.59%), Space: 61.8 MB (70.95%) - LeetHub

---
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        
+        int n = nums.length;
+        int maxNum = Arrays.stream(nums).max().getAsInt();
+        //num -> frequency
+        Map<Integer, Integer> map = new HashMap();
+        int start = 0;
+        long res = 0;
+        for (int end = 0; end < n; end++) {
+            map.put(nums[end], map.getOrDefault(nums[end], 0) + 1);
+            if (map.getOrDefault(maxNum, 0) >= k) {
+                // the num of appropriate array with start as its left border
+                res += (n - end);
+            }
+            
+            while (map.getOrDefault(maxNum, 0) >= k) {
+                map.put(nums[start], map.get(nums[start]) - 1);
+                if (map.get(maxNum) >= k) {
+                    res += (n - end);
+                }
+                start++;
+            }
+        }
+        
+        return res;
+    }
+}

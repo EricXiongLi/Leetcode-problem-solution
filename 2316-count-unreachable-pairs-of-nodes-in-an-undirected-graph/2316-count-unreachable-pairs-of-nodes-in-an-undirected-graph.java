21300240e2895318188670d120005424291d3568
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-07-20 12:34:17 +0200

    Time: 132 ms (14.07%), Space: 124.7 MB (16.87%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -0,0 +1,48 @@
+class Solution {
+    public long countPairs(int n, int[][] edges) {
+        Map<Integer, List<Integer>> graph = new HashMap();
+        List<Long> group = new ArrayList();
+        for (int[] edge : edges) {
+            int start = edge[0];
+            int end = edge[1];
+            graph.putIfAbsent(start, new ArrayList());
+            graph.putIfAbsent(end, new ArrayList());
+            graph.get(start).add(end);
+            graph.get(end).add(start);
+        }
+        Set<Integer> visited = new HashSet();
+        
+        for (int i = 0; i< n; i++) {
+            if (!visited.contains(i)) {
+                group.add(countSize(i, visited, graph));
+            }
+        }
+        int l = group.size();
+        long res = 0L;
+        
+        long sum = group.stream().mapToLong(Long::longValue).sum();
+        for (long num : group) {
+            res += (num * (sum - num));
+        }
+        
+        return res / 2;
+    }
+    
+    public long countSize(int i, Set<Integer> visited, Map<Integer, List<Integer>> graph) {
+        Deque<Integer> q = new ArrayDeque();
+        q.offer(i);
+        visited.add(i);
+        long size = 0;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            size++;
+            for (int nei : graph.getOrDefault(cur, new ArrayList<Integer>())) {
+                if (!visited.contains(nei)) {
+                    q.offer(nei);
+                    visited.add(nei);
+                }
+            }
+        }
+        return size;
+    }
+}

bbe2d055ac6b9dd49631e39fc95ba1e346eb9995
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-07-23 22:48:17 +0200

    Time: 5 ms (87.06%), Space: 45.9 MB (11.27%) - LeetHub

---
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for Node.
+ * public class Node {
+ *     int val;
+ *     Node left;
+ *     Node right;
+ *     Node random;
+ *     Node() {}
+ *     Node(int val) { this.val = val; }
+ *     Node(int val, Node left, Node right, Node random) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *         this.random = random;
+ *     }
+ * }
+ */
+
+class Solution {
+    Map<Node, NodeCopy> map = new HashMap();
+    
+    public NodeCopy copyRandomBinaryTree(Node root) {
+        return dfs(root);
+    }
+    
+    public NodeCopy dfs(Node root) {
+        if (root == null) {
+            return null;
+        }
+        if (map.containsKey(root)) {
+            return map.get(root);
+        }
+        NodeCopy copy = new NodeCopy(root.val);
+        map.put(root, copy);
+        copy.left = dfs(root.left);
+        copy.right = dfs(root.right);
+        copy.random = dfs(root.random);
+        return copy;
+    }
+}

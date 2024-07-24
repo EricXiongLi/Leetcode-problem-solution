ebf3f8558c922a1799675f70d4e8d032b1469af8
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-07-24 11:13:16 +0200

    Time: 0 ms (100.00%), Space: 41.6 MB (60.27%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isEqual(root.left, root.right);
+    }
+    
+    public boolean isEqual(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 != null && node2 != null) {
+            return node1.val == node2.val && isEqual(node1.left, node2.right) && isEqual(node1.right, node2.left);
+        }
+        
+        return false;
+    }
+}

e8e8031428bc3e9d848d29e4379859fa03c03ccc
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-07-31 11:46:24 +0200

    Time: 2 ms (100.00%), Space: 50.1 MB (44.72%) - LeetHub

---
+++ 0370-range-addition/0370-range-addition.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        int n = length;
+        int[] res = new int[n];
+        for (int[] update : updates) {
+            res[update[0]] += update[2];
+            if (update[1] + 1 < n) {
+                res[update[1] + 1] -= update[2];
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            res[i] += i == 0 ? 0 :res[i- 1];
+        }
+        
+        return res;
+    }
+}

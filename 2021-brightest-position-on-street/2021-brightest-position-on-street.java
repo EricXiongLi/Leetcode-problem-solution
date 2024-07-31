612928f602342b37b8d98f66e1e9bd43e2d49cbc
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-07-31 11:34:28 +0200

    Time: 103 ms (58.82%), Space: 79.6 MB (29.41%) - LeetHub

---
+++ 2021-brightest-position-on-street/2021-brightest-position-on-street.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int brightestPosition(int[][] lights) {
+        List<int[]> list = new ArrayList();
+        for (int[] light : lights) {
+            list.add(new int[]{light[0] - light[1], 1});
+            list.add(new int[]{light[0] + light[1] + 1, -1});
+        }
+        
+        Collections.sort(list, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        int max = Integer.MIN_VALUE;
+        int cur = 0;
+        int res = -1;
+        for (int[] item : list) {
+            cur += item[1];
+            if (cur > max) {
+                res = item[0];
+                max = cur;
+            }
+        }
+        
+        return res;
+    }
+}

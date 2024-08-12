03c15e2347d8eb347acb03bac9bc1557d1b9cfff
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-08-12 14:06:44 +0200

    Time: 0 ms (100.00%), Space: 45.6 MB (77.33%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0, end = s.length - 1;
+        while (start <= end) {
+            char tmp = s[start];
+            s[start] = s[end];
+            s[end] = tmp;
+            start++;
+            end--;
+        }
+        
+    }
+}

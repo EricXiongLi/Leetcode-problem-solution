b66760ef74f64bc2991a17aa793c38c4e294a8a1
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-09-20 22:26:07 +0200

    Time: 2 ms (98.83%), Space: 41.6 MB (77.13%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
@@ -1,17 +1,17 @@
 class Solution {
     public int twoSumLessThanK(int[] nums, int k) {
         Arrays.sort(nums);
-        int n = nums.length;
-        int l = 0, r = n - 1;
-        int res = -1;
-        while (l < r) {
-            if (nums[l] + nums[r] < k) {
+        int l = 0, r = nums.length - 1;
+        int res = Integer.MIN_VALUE;
+        while (r > l) {
+            if (nums[l] + nums[r] >= k) {
+                r--;
+            } else {
                 res = Math.max(res, nums[l] + nums[r]);
                 l++;
-            } else {
-                r--;
             }
         }
-        return res;
+        
+        return res == Integer.MIN_VALUE ? -1 : res;
     }
 }

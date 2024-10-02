779f89696ddf4bb6a96cec3e0a5e43a43d790eeb
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-10-02 10:50:40 +0200

    Time: 14 ms (35.00%), Space: 65.1 MB (44.29%) - LeetHub

---
+++ 2361-minimum-costs-using-the-train-line/2361-minimum-costs-using-the-train-line.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long[] minimumCosts(int[] regular, int[] express, int expressCost) {
+        //dp[i][0] = Math.min(dp[i-1][0] + regular[i], dp[i - 1][1] + regular[i], dp[i - 1][1] + express[i])
+        //dp[i][1] = Math.min(dp[i - 1][1] + express[i], dp[i - 1][0] + expressCost + express[i], dp[i - 1][0] + regular[i] + expressCost);
+        int n = regular.length;
+        long[] dp = new long[2];
+        long[] prevDp = new long[2];
+        prevDp[1] = expressCost;
+        long[] res = new long[n];
+        
+        for (int i = 0; i < n; i++) {
+            dp[0] = Math.min(Math.min(prevDp[0] + regular[i], prevDp[1] + regular[i]), prevDp[1] + express[i]);
+            dp[1] = Math.min(Math.min(prevDp[1] + express[i], prevDp[0] + expressCost + express[i]), prevDp[0] + regular[i] + expressCost);
+            res[i] = Math.min(dp[0], dp[1]);
+            prevDp[0] = dp[0];
+            prevDp[1] = dp[1];
+        }
+        
+        return res;
+    }
+}

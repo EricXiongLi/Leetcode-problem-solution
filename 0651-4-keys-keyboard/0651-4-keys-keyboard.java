4538bb474b12140810a2ddceae08d9822e91e247
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-10-06 12:10:39 +0200

    Time: 0 ms (100.00%), Space: 40.2 MB (75.18%) - LeetHub

---
+++ 0651-4-keys-keyboard/0651-4-keys-keyboard.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxA(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (i <= 5) {
+                dp[i] = i;
+            } else {
+                dp[i] = Math.max(dp[i - 3] * 2, Math.max(dp[i - 4] * 3, dp[i - 5] * 4));
+            }
+        }
+        
+        return dp[n];
+    }
+}
+
+//tc: O(n)
+//sc: O(n)

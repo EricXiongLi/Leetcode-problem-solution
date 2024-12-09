0c2ce4bff42ec579a0091b86e21c266784f8cb5b
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-12-09 23:27:41 +0100

    Time: 2187 ms (5.10%), Space: 64.8 MB (5.98%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,33 +1,38 @@
 class Solution {
+    
     public int findKthLargest(int[] nums, int k) {
         int n = nums.length;
-        k = n - k;
-        return quickSelect(nums, k, 0, n - 1);
+        return quicksort(nums, 0, n - 1, n - k + 1);
     }
     
-    public int quickSelect(int[] nums, int k, int l, int r) {
-        int ran  = l + (int) (Math.random() * (r - l));
-        swap(nums, ran, r);
-        int pivot = nums[r], p = l;
-        for (int i = l; i < r; i++) {
+    int quicksort(int[] nums, int l, int r, int k) {
+        int randomIndex = l + (int)(Math.random() * (r - l + 1));
+        int pivot = partition(nums, l, r, randomIndex);
+        if (pivot == k - 1) return nums[pivot];
+        if (k - 1 > pivot) {
+            return quicksort(nums, pivot + 1, r, k);
+        } else {
+            return quicksort(nums, l, pivot - 1, k);
+        }
+    }
+    
+    int partition(int[] nums, int l, int r, int pivotId) {
+        int pivot = nums[pivotId];
+        swap(nums, r, pivotId);
+        int wall = l;
+        for (int i = l; i <= r - 1; i++) {
             if (nums[i] < pivot) {
-                swap(nums, i, p);
-                p++;
+                swap(nums, i, wall);
+                wall++;
             }
         }
-        swap(nums, r, p);
-        if (p == k) {
-            return nums[p];
-        } else if (p < k) {
-            return quickSelect(nums,k, p + 1, r);
-        } else {
-            return quickSelect(nums, k, l, p - 1);
-        }
+        swap(nums, wall, r);
+        return wall;
     }
     
-    public void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    void swap(int[] nums, int l, int r) {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
     }
 }

50a962438e19b3afc6d76b237b37d62888a0e327
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2024-12-16 21:37:19 +0100

    Time: 148 ms (23.46%), Space: 46.7 MB (48.15%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkPartitioning(String s) {
+        int n = s.length();
+        //is string[i, j] is palindrome
+        //dp[i][j] = flase ? dp[i + 1][j - 1]
+        boolean[][] dp = new boolean[n][n];
+        for (boolean[] arr : dp) {
+            Arrays.fill(arr, true);
+        }
+        for (int r = n - 1; r >= 0; r--) {
+            for (int c = r; c <= n - 1; c++) {
+                if (r == c) {
+                    dp[r][c] = true;
+                } else {
+                    dp[r][c] = s.charAt(r) != s.charAt(c) ? false : dp[r + 1][c - 1];
+                }
+            }
+        }
+        
+        //dp[i][j] = dp[i][m] + dp[m][k] + dp[k][j]
+        for (int len1 = 1; 0 + len1 - 1 <= n - 3; len1++) {
+            for (int len2 = 1; 0 + len1 - 1 + 1 + len2 - 1 <= n - 2; len2++) {
+                if (dp[0][0 + len1 - 1] && dp[len1][len1 + len2 - 1] && dp[len1 + len2][n - 1]) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+        
+    }
+}

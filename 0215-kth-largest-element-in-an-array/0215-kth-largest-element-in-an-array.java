69c5e3907868142b7c9c3713c5e4d92fb183f30d
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-04-06 10:58:19 +0100

    Time: 37 ms (73.2%), Space: 58.6 MB (45.82%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,38 +1,36 @@
 class Solution {
-    
     public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
-        return quicksort(nums, 0, n - 1, n - k + 1);
-    }
-    
-    int quicksort(int[] nums, int l, int r, int k) {
-        int randomIndex = l + (int)(Math.random() * (r - l + 1));
-        int pivot = partition(nums, l, r, randomIndex);
-        if (pivot == k - 1) return nums[pivot];
-        if (k - 1 > pivot) {
-            return quicksort(nums, pivot + 1, r, k);
-        } else {
-            return quicksort(nums, l, pivot - 1, k);
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
         }
+        return quickselect(list, nums.length + 1 - k);
     }
-    
-    int partition(int[] nums, int l, int r, int pivotId) {
-        int pivot = nums[pivotId];
-        swap(nums, r, pivotId);
-        int wall = l;
-        for (int i = l; i <= r - 1; i++) {
-            if (nums[i] < pivot) {
-                swap(nums, i, wall);
-                wall++;
+
+
+    public int quickselect(List<Integer> list, int k) {
+        List<Integer> less = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> greater = new ArrayList<>();
+
+        int pivotId = new Random().nextInt(list.size());
+        int pivot = list.get(pivotId);
+        for (int num : list) {
+            if (num < pivot) {
+                less.add(num);
+            } else if (num == pivot) {
+                equal.add(num);
+            } else {
+                greater.add(num);
             }
         }
-        swap(nums, wall, r);
-        return wall;
-    }
-    
-    void swap(int[] nums, int l, int r) {
-        int temp = nums[l];
-        nums[l] = nums[r];
-        nums[r] = temp;
+
+        if (less.size() >= k) {
+            return quickselect(less, k);
+        } else if (k > less.size() + equal.size()) {
+            return quickselect(greater, k - less.size() - equal.size());
+        } else {
+            return pivot;
+        }
     }
 }

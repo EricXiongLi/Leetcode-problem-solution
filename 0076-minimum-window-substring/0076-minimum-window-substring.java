c6309d84de00b9d3dd66bf6fad6b386a474cd0cc
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-04-09 11:06:03 +0100

    Time: 21 ms (37.02%), Space: 45.5 MB (28.14%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,44 +1,45 @@
 class Solution {
     public String minWindow(String s, String t) {
         Map<Character, Integer> tmap = new HashMap<>();
-        Map<Character, Integer> smap = new HashMap<>();
-        int targetCount = t.length();
-        int minL = 0;
-        int minLen = Integer.MAX_VALUE;
+
         for (char c : t.toCharArray()) {
             tmap.put(c, tmap.getOrDefault(c, 0) + 1);
         }
-        
+        int targetCount = t.length();
+        int resL = -1;
+        int resR = 100001;
         int l = 0;
         int count = 0;
+
+        Map<Character, Integer> cur = new HashMap<>();
         for (int r = 0; r < s.length(); r++) {
             char c = s.charAt(r);
             if (tmap.containsKey(c)) {
-                if (smap.getOrDefault(c, 0) < tmap.get(c)) {
+                if (cur.getOrDefault(c, 0) < tmap.get(c)) {
                     count++;
                 }
-                smap.put(c, smap.getOrDefault(c, 0) + 1);
+                cur.put(c, cur.getOrDefault(c, 0) + 1);
             }
-            while (count == targetCount) {
-                if (r - l + 1 < minLen) {
-                    minLen = r - l + 1;
-                    minL = l;
+            while (count >= targetCount) {
+                if (r - l < resR - resL) {
+                    resL = l;
+                    resR = r;
                 }
                 char leftC = s.charAt(l);
                 if (tmap.containsKey(leftC)) {
-                    smap.put(leftC, smap.get(leftC) - 1);
-                    if (smap.get(leftC) < tmap.get(leftC)) {
+                    if (cur.get(leftC) <= tmap.get(leftC)) {
                         count--;
                     }
-                    if (smap.get(leftC) == 0) smap.remove(leftC);
+                    cur.put(leftC, cur.get(leftC) - 1);
+                    if (cur.get(leftC) == 0) {
+                        cur.remove(leftC);
+                    }
                 }
                 l++;
             }
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minL, minL + minLen);
-        
+
+        if (resL == -1) return "";
+        return s.substring(resL, resR + 1);
     }
 }
-
-//tc: O(m + n)
-//sc: O(m + n)

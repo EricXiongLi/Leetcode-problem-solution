25318b95883fae8ec75a13824f31b2ba1a14a5d7
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-04-10 16:42:01 +0100

    Time: 1 ms (99.88%), Space: 46.5 MB (17.36%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int res = 0;
+
         for (int num : nums) {
             res ^= num;
         }
-        
+
         return res;
     }
 }

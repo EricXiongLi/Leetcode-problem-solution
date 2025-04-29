059c3d8946de8696b9637fd268827766400fa509
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-04-29 05:17:41 +0100

    Time: 0 ms (100%), Space: 40.5 MB (50.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        //dp[i]: the ways to get n th stair
-        //dp[i] = dp[i - 1] + dp[i - 2]
-        if (n == 1) return 1;
-        int[] dp = new int[n];
-        int a = 1;
-        int b = 2;
-        int c = 0;
-        for (int i = 2; i < n; i++) {
+        //ddp[n] = dp[n -1] + dp[n - 2];
+        int a = 1, b = 1;
+        if (n == 0 || n == 1) return 1;
+        int c = 2;
+        for (int i = 2; i <= n; i++) {
             c = a + b;
             a = b;
             b = c;
         }
-        return b;
+
+        return c;
     }
 }
-
-//tc: O(n)
-//sc: O(1)

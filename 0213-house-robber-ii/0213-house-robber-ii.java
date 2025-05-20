50f4c29df9283168cf32369766a5e0d79dc1ea15
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-05-20 12:47:38 +0800

    Time: 0 ms (100%), Space: 41.1 MB (51.34%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,25 +1,24 @@
 class Solution {
-    int[] dp;
     public int rob(int[] nums) {
         int n = nums.length;
+
         if (n == 1) return nums[0];
-        int rob1 = rob(nums, 0, n - 2);
-        int rob2 = rob(nums, 1, n - 1);
-        return Math.max(rob1, rob2);
+        int res1 = rob(nums, 0, n - 2);
+        int res2 = rob(nums, 1, n - 1);
+
+        return Math.max(res1, res2);
     }
-    
-    int rob(int[] nums, int l, int r) {
-        int prev1 = 0;
-        int prev2 = 0;
-        int cur = 0;
-        for (int i = r; i >= l; i--) {
-            cur = Math.max(prev1, nums[i] + prev2);
-            prev2 = prev1;
-            prev1 = cur;
+
+    public int rob(int[] nums, int l, int r) {
+        int[] dp = new int[r - l + 1];
+        dp[0] = nums[l];
+        if (dp.length == 1) return dp[0];
+        dp[1] = Math.max(nums[l], nums[l + 1]);
+
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i + l], dp[i - 1]);
         }
-        return cur;
+
+        return dp[r - l];
     }
 }
-
-//tc: O(n)
-//sc: O(n)

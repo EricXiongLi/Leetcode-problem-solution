9bc9cd555de4e75469b221b85628aeaf5445c1d3
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-05-20 12:33:23 +0800

    Time: 0 ms (100%), Space: 40.8 MB (95.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,12 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        //dp[i]: starting robbing from ith house
-        //dp[i] = Math.max(dp[i + 1], dp[i + 2] + nums[i]);
+        //dp[i]: the max profit within [0...i]
+        //dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
         int n = nums.length;
-        int[] dp = new int[n + 2];
-        for (int i = n - 1; i >= 0; i--) {
-            dp[i] = Math.max(dp[i + 1], dp[i + 2] + nums[i]);
+        if (n == 1) return nums[0];
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
         }
-        return dp[0];
+
+        return dp[n - 1];
     }
 }

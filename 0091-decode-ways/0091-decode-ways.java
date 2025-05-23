bc45182c4a5869bd2fc92cc6c78c4f8034a62879
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-05-23 12:35:51 +0800

    Time: 1 ms (84.4%), Space: 41.7 MB (87.46%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,23 +1,39 @@
 class Solution {
     public int numDecodings(String s) {
-        //dp[i] : the ways of s[0, i)
-        //if s.charAt(i - 1) != '0': dp[i] += dp[i - 1]
-        //if s.substring(i - 2, i) is valid: dp[i] += dp[i - 2]
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        if (s.charAt(0) != '0') dp[1] = 1;
-        else dp[1] = 0;
-        for (int i = 2; i <= n; i++) {
-            if (s.charAt(i - 1) != '0') dp[i] += dp[i - 1];
-            if (isValid(s.substring(i - 2, i))) dp[i] += dp[i - 2];
-        }
-        return dp[n];
-        
+        //dp[i]: the possible # of combination for string[i...n]
+        //dp[i] = s[i] is 0 ? 0 :  s[i...i + 1] is a possible string, which is from 10 to 26 ? dp[i + 1] + dp[i + 2] : dp[i + 1]
+
+    //267
+    //string[0...1]: 26 -> possible : dp[1] (67) + dp[2](7)
+
+    int n = s.length();
+    int[] dp = new int[n];
+
+    //dp[6] = 1
+    dp[n - 1] = s.charAt(n - 1) == '0' ? 0 : 1;
+    if (n == 1) return dp[0];
+    dp[n - 2] = s.charAt(n - 2) == '0' ? 0 : (isPossible(s, n - 2) ? dp[n - 1] + 1 : dp[n - 1]);
+    //dp[5] = 1
+    for (int i = n - 3; i >= 0; i--) {
+        dp[i] = s.charAt(i) == '0' ? 0 : ((isPossible(s, i) ? dp[i + 1] + dp[i + 2] : dp[i + 1]));
     }
-    
-    public boolean isValid(String s) {
-        int val = Integer.valueOf(s);
-        return val >= 1 && val <= 26 && s.charAt(0) != '0';
+
+    //dp[4] = 2
+
+    //dp[3] = 3
+
+    //dp[2] = 0
+
+    //dp[1] = 0 + 3 = 3
+
+    //dp[0] = 
+    return dp[0];
+    }
+
+    public boolean isPossible(String s, int i) {
+        int val = Integer.valueOf(s.substring(i, i + 2));
+
+        return val >= 10 && val <= 26;
     }
 }
+

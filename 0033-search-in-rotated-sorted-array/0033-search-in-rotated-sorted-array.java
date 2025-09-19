74bdc5cbd34bd846600faefbd1f99b26fcb3a8b3
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-20 00:06:38 +0800

    Time: 0 ms (100%), Space: 42.2 MB (40.44%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,12 +1,17 @@
+/*
+ * @lc app=leetcode id=33 lang=java
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
 class Solution {
     public int search(int[] nums, int target) {
         int l = 0, r = nums.length - 1;
-        int n = nums.length;
         while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (target == nums[m]) {
-                return m;
-            }
+            int m = l + (r - l)/2;
+            if (nums[m] == target) return m;
+
             if (nums[m] >= nums[0]) {
                 if (target >= nums[0] && target < nums[m]) {
                     r = m - 1;
@@ -14,7 +19,7 @@ class Solution {
                     l = m + 1;
                 }
             } else {
-                if (target > nums[m] && target <= nums[n - 1]) {
+                if (target > nums[m] && target <= nums[r]) {
                     l = m + 1;
                 } else {
                     r = m - 1;
@@ -25,3 +30,5 @@ class Solution {
         return -1;
     }
 }
+// @lc code=end
+

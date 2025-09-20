ae1e60813427d1fec318be624cc382743e277651
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-21 04:34:16 +0800

    Time: 1 ms (99.46%), Space: 57.7 MB (8.21%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //dp[i]: max sum ending with nums[i]
-        //dp[i] = dp[i - 1] >= 0 ? dp[i - 1] + nums[i] : nums[i];
-
-        int n = nums.length;
-        if (n == 1) return nums[0];
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        int res = dp[0];
-        for (int i = 1; i < n; i++) {
-            dp[i] = dp[i - 1] >= 0 ? dp[i - 1] + nums[i] : nums[i];
-            res = Math.max(dp[i], res);
+        int res = nums[0];
+        int greed = 0;
+        for (int num : nums) {
+            greed = greed >= 0 ? num + greed : num;
+            res = Math.max(res, greed);
         }
 
         return res;

5ebbc98e7d135934f153d46da8d4f6969fbe9316
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-20 23:22:59 +0800

    Time: 0 ms (100%), Space: 42.4 MB (6.95%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,23 +15,26 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        return isEqual(root.left, root.right);
+        if (root == null) return true;
+        return isSymmetric(root.left, root.right);
     }
-    
-    public boolean isEqual(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) {
+
+    public boolean isSymmetric(TreeNode a, TreeNode b) {
+        if (a == null && b == null) {
             return true;
+        } else if (a == null && b != null) {
+            return false;
+        } else if (a != null && b == null) {
+            return false;
+        } else {
+            if (a.val != b.val) {
+                return false;
+            } else {
+                return isSymmetric(a.left, b.right) && isSymmetric(a.right, b.left);
+            }
         }
-        if (node1 != null && node2 != null) {
-            return node1.val == node2.val && isEqual(node1.left, node2.right) && isEqual(node1.right, node2.left);
-        }
-        
-        return false;
     }
 }
 
 //tc: O(n)
-//sc: O(n) worst case
+//sc: O(h) -> O(n)

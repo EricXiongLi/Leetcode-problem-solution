bc51e16feb9a3225036167f524b00100c703df9a
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-20 22:45:16 +0800

    Time: 8 ms (14.44%), Space: 45.9 MB (65.51%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -2,36 +2,41 @@ class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
         int[] indegree = new int[numCourses];
-
-        for (int[] pre : prerequisites) {
-            int from = pre[1], to = pre[0];
-            graph.computeIfAbsent(from, v -> new LinkedList<Integer>()).add(to);
-            indegree[to]++;
-        } 
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < indegree.length; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
-            }
-        }
-
         List<Integer> res = new LinkedList<>();
 
+        for (int[] pre : prerequisites) {
+            int src = pre[1], dest = pre[0];
+            graph.computeIfAbsent(src, v -> new LinkedList<Integer>()).add(dest);
+            indegree[dest]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) q.offer(i);
+        }
+
         while (!q.isEmpty()) {
-            int cur =q.poll();
+            int size = q.size();
 
-            res.add(cur);
+            for (int i = 0; i < size; i++) {
+                int cur = q.poll();
+                res.add(cur);
 
-            for (int nei : graph.getOrDefault(cur, new LinkedList<Integer>())) {
-                indegree[nei]--;
-                if (indegree[nei] == 0) {
-                    q.offer(nei);
+                for (int nei : graph.getOrDefault(cur, new LinkedList<Integer>())) {
+                    indegree[nei]--;
+                    if (indegree[nei] == 0) {
+                        q.offer(nei);
+                    }
                 }
             }
         }
+
         if (res.size() != numCourses) return new int[0];
         return res.stream().mapToInt(i -> i).toArray();
+
     }
 }
+
+//tc: O(V + E)
+
+//sc: O(V + E)

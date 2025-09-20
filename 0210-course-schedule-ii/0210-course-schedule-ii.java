1ec11ede9f650611c5091e6c41747d53669ef4dd
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-20 22:42:19 +0800

    Time: 8 ms (14.44%), Space: 46.1 MB (54.5%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,44 +1,37 @@
 class Solution {
-    public int[] findOrder(int numcourses, int[][] prerequisites) {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
-
-        int[] indegree = new int[numcourses];
+        int[] indegree = new int[numCourses];
 
         for (int[] pre : prerequisites) {
             int from = pre[1], to = pre[0];
+            graph.computeIfAbsent(from, v -> new LinkedList<Integer>()).add(to);
             indegree[to]++;
-            graph.computeIfAbsent(from, v -> new ArrayList<Integer>()).add(to);
-            graph.computeIfAbsent(to, v -> new ArrayList<Integer>()).add(from);
-        }
+        } 
 
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numcourses; i++) {
+
+        for (int i = 0; i < indegree.length; i++) {
             if (indegree[i] == 0) {
                 q.offer(i);
             }
         }
 
-        int[] res = new int[numcourses];
-
-        int p = 0;
-        int count = 0;
+        List<Integer> res = new LinkedList<>();
 
         while (!q.isEmpty()) {
-            int sz = q.size();
-            for (int i = 0; i < sz; i++) {
-                int cur = q.poll();
-                res[p++] = cur;
-                count++;
+            int cur =q.poll();
 
-                for (int nei : graph.getOrDefault(cur, new ArrayList<Integer>())) {
-                    indegree[nei]--;
-                    if (indegree[nei] == 0) {
-                        q.offer(nei);
-                    }
+            res.add(cur);
+
+            for (int nei : graph.getOrDefault(cur, new LinkedList<Integer>())) {
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    q.offer(nei);
                 }
             }
         }
-
-        return count == numcourses ? res : new int[0];
+        if (res.size() != numCourses) return new int[0];
+        return res.stream().mapToInt(i -> i).toArray();
     }
 }

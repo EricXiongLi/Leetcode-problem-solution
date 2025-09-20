0506080fe5b9e99e4beff0654b1927fd5a36b7ab
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-20 22:11:36 +0800

    Time: 83 ms (78.45%), Space: 46.2 MB (90.87%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Set<String> dead = new HashSet<>(Arrays.asList(deadends));
+        Set<String> visited = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+
+        q.offer("0000");
+        visited.add("0000");
+        if (target.equals("0000")) return 0;
+        if (dead.contains("0000") && !target.equals("0000")) return -1;
+        int step = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                String cur = q.poll();
+
+                for (int j = 0; j < 4; j++) {
+                    String plus = plusOne(cur, j);
+                    String minus = minusOne(cur, j);
+                    if (plus.equals(target) || minus.equals(target)) {
+                        return step + 1;
+                    }
+
+                    if (!visited.contains(plus) && !dead.contains(plus)) {
+                        visited.add(plus);
+                        q.offer(plus);
+                    }
+
+                    if (!visited.contains(minus) && !dead.contains(minus)) {
+                        visited.add(minus);
+                        q.offer(minus);
+                    }
+                    
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+
+    public String plusOne(String s, int j) {
+        char[] chs = s.toCharArray();
+
+        if (chs[j] == '9') {
+            chs[j] = '0';
+        } else {
+            chs[j] += 1;
+        }
+
+        return new String(chs);
+    }
+
+    public String minusOne(String s, int j) {
+        char[] chs = s.toCharArray();
+        if (chs[j] == '0') {
+            chs[j] = '9';
+        } else {
+            chs[j] -= 1;
+        }
+
+        return new String(chs);
+    }
+}

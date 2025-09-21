cba94b29fa7f85b7e45acc18d88d6feb12f3d219
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-22 02:44:10 +0800

    Time: 6 ms (22.97%), Space: 52.5 MB (11.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,15 +1,17 @@
 class Solution {
-    private static int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    int[][] dirs = new int[][] {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int m = grid.length, n = grid[0].length;
         int count = 0;
+        boolean[][] visited = new boolean[m][n];
+
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
-                if (grid[r][c] == '1') {
-                    count++;
-                    bfs(grid, m, n, r, c);
+                if (grid[r][c] == '1' && !visited[r][c]) {
+                    visited[r][c] = true;
+                    bfs(grid, r, c, visited, m, n);
+                    count++;                   
                 }
             }
         }
@@ -17,22 +19,22 @@ class Solution {
         return count;
     }
 
-    public void bfs(char[][] grid, int m, int n, int r, int c) {
+    public void bfs(char[][] grid, int r, int c, boolean[][] visited, int m, int n) {
         Queue<int[]> q = new LinkedList<>();
-
         q.offer(new int[]{r, c});
-        grid[r][c] = '2';
+
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             for (int[] dir : dirs) {
-                int neiR = dir[0] + cur[0];
-                int neiC = dir[1] + cur[1];
+                int newRow = dir[0] + cur[0], newCol = dir[1] + cur[1];
 
-                if (neiR >= 0 && neiR < m && neiC >= 0 && neiC < n && grid[neiR][neiC] == '1') {
-                    grid[neiR][neiC] = '2';
-                    q.offer(new int[]{neiR, neiC});
+                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !visited[newRow][newCol] && grid[newRow][newCol] == '1') {
+                    q.offer(new int[]{newRow, newCol});
+                    visited[newRow][newCol] = true;
                 }
             }
         }
     }
+
+
 }

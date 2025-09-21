9141a49c933bb9584ae38267d3d1576ecdb35780
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-22 03:35:55 +0800

    Time: 10 ms (13.17%), Space: 45.7 MB (25.89%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,55 +1,35 @@
 class Solution {
     public int countComponents(int n, int[][] edges) {
-        DSU dsu = new DSU(n);
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
         for (int[] edge : edges) {
-            dsu.union(edge[0], edge[1]);
+            graph.computeIfAbsent(edge[0], v -> new ArrayList<Integer>()).add(edge[1]);
+            graph.computeIfAbsent(edge[1], v -> new ArrayList<Integer>()).add(edge[0]);
         }
 
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
         int count = 0;
-
         for (int i = 0; i < n; i++) {
-            if (dsu.find(i) == i) {
+            if (!visited[i]) {
+                //bfs
                 count++;
+                q.offer(i);
+                visited[i] = true;
+                while (!q.isEmpty()) {
+                   int cur = q.poll();
+
+                   for (int nei : graph.getOrDefault(cur, new ArrayList<Integer>())) {
+                        if (!visited[nei]) {
+                            q.offer(nei);
+                            visited[nei] = true;
+                        }
+                   }
+                }
+                
             }
         }
 
         return count;
-    }
-
-    public class DSU {
-        public int[] parents;
-        public int N;
-        public int[] size;
-
-        public DSU(int N) {
-            this.N = N;
-            size = new int[N];
-            parents = new int[N];
-            for (int i = 0; i < N; i++) {
-                parents[i] = i;
-            }
-
-            Arrays.fill(size, 1);
-        }
-
-        public int find(int x) {
-            if (parents[x] != x) {
-                parents[x] = find(parents[x]);
-            }
-            return parents[x];
-        }
-
-        public void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX == rootY) return;
-            if (size[rootX] < size[rootY]) {
-                parents[rootX] = rootY;
-                size[rootY] += size[rootX];
-            } else {
-                parents[rootY] = rootX;
-                size[rootX] += size[rootY];
-            }
-        }
     }
 }

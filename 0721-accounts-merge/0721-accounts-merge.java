03e8d4428c87ce46d953109ff948e45269d56943
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-23 05:08:55 +0800

    Time: 34 ms (41.64%), Space: 49.3 MB (24.62%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,76 +1,84 @@
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        Map<Integer, Set<String>> idToEmails = new HashMap<>();
-        Map<String, Integer> emailToId = new HashMap<>();
-        int size = accounts.size();
-        DSU dsu = new DSU(size);
-        for (int i = 0; i < size; i++) {
-            List<String> list = accounts.get(i);
-            for (int j = 1; j < list.size(); j++) {
-                String email = list.get(j);
-                idToEmails.computeIfAbsent(i, v -> new HashSet<String>()).add(list.get(j));
-                if (emailToId.containsKey(email)) {
-                    int oldId = emailToId.get(email);
-                    dsu.union(oldId, i);
+        //email to #
+        Map<String, Integer> emailsMap = new HashMap<>();
+        //# to name
+        Map<Integer, String> namesMap = new HashMap<>();
+        DSU dsu = new DSU(accounts.size());
+
+        for (int i = 0; i < accounts.size(); i++) {
+            namesMap.put(i, accounts.get(i).get(0));
+            for (int j = 1; j < accounts.get(i).size(); j++) {
+                String email = accounts.get(i).get(j);
+
+                if (emailsMap.containsKey(email)) {
+                    dsu.union(i, emailsMap.get(email));
+                } else {
+                    emailsMap.put(email, i);
                 }
-                emailToId.put(list.get(j), i);
             }
         }
-        
-        for (int i = 0; i < size; i++) {
-            int parent = dsu.find(i);
-            if (parent != i) {
-                Set<String> rootEmails = idToEmails.get(parent);
-                rootEmails.addAll(idToEmails.get(i));
+
+        Map<Integer, Set<String>> allEmailsMap = new HashMap<>();
+
+        for (int i = 0; i < accounts.size(); i++) {
+            int root = dsu.find(i);
+            if (!allEmailsMap.containsKey(root)) {
+                allEmailsMap.put(root, new HashSet<String>());
+            }
+            for (int j = 1; j < accounts.get(i).size(); j++) {
+                allEmailsMap.get(root).add(accounts.get(i).get(j));
             }
         }
-        
-        List<List<String>> res = new ArrayList<>();
-        
-        for (int i = 0; i < size; i++) {
-            int parent = dsu.find(i);
-            String name = accounts.get(i).get(0);
-            if (parent == i) {
-                List<String> emails = new ArrayList<>(idToEmails.get(i));
-                Collections.sort(emails);
-                emails.add(0, name);
-                res.add(emails);
-            }
+
+        List<List<String>> res = new LinkedList<>();
+
+        for (int key : allEmailsMap.keySet()) {
+            List<String> cur = new LinkedList<>();
+            cur.add(namesMap.get(key));
+            List<String> emails = new ArrayList<>(allEmailsMap.get(key));
+            Collections.sort(emails);
+            cur.addAll(emails);
+            res.add(cur);
         }
+
         return res;
+        
     }
 }
 
 class DSU {
     int[] parent;
     int[] size;
-    
-    public DSU (int n) {
+
+    public DSU(int n) {
         parent = new int[n];
         size = new int[n];
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
+
         Arrays.fill(size, 1);
     }
-    
+
     public int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
+        int parentX = parent[x];
+
+        if (x != parentX) {
+            parent[x] = find(parentX);
         }
         return parent[x];
     }
-    
+
     public void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
+        int rootX = find(x), rootY = find(y);
         if (rootX == rootY) return;
-        if (size[rootX] < size[rootY]) {
-            parent[rootX] = rootY;
+        if (size[rootX] <= size[rootY]) {
             size[rootY] += size[rootX];
-        } else {
-            parent[rootY] = rootX;
+            parent[rootX] = rootY;
+        } else if (size[rootX] > size[rootY]) {
             size[rootX] += size[rootY];
+            parent[rootY] = rootX;
         }
     }
 }

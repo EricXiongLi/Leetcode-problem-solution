2a2d1eee1332e10683d3dff97fa0fbe695955962
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-22 22:21:37 +0800

    Time: 5 ms (28.86%), Space: 45 MB (38.73%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,47 +1,50 @@
 class Solution {
+    List<List<String>> res = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res = new ArrayList<>();
-        List<StringBuilder> cur = new ArrayList<>();
-    
+        //diagnoal1: r + c
+        //diagonal2: c-  r
+        Set<Integer> diag1 = new HashSet<>(), diag2 = new HashSet<>();
+        boolean[] fulfilledRow = new boolean[n], fulfilledCol = new boolean[n];
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < n; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                sb.append(".");
-            }
-            cur.add(sb);
+            sb.append(".");
         }
-        boolean[] colUsed = new boolean[n];
-        boolean[] diag1Used = new boolean[2 * n - 1], diag2Used = new boolean[2 * n - 1];
-        dfs(res, cur, 0, colUsed, diag1Used, diag2Used, n);
-        return res;
-    }
-    
-    public void dfs(List<List<String>> res, List<StringBuilder> cur, int row, boolean[] colUsed, boolean[] diag1Used, boolean[] diag2Used, int n) {
-        if (row == n) {
-            List<String> curList = new ArrayList<>();
-            
-            for (StringBuilder sb : cur) {
-                curList.add(sb.toString());
-            }
-            res.add(curList);
-            return;
+        List<StringBuilder> path = new ArrayList<StringBuilder>();
+
+        for (int i = 0; i < n; i++) {
+            path.add(new StringBuilder(sb));
         }
         
+        backtracking(0, n, path, diag1, diag2, fulfilledCol);
+        return res;
+    }
+
+    public void backtracking(int row, int n, List<StringBuilder> path, Set<Integer> diag1, Set<Integer> diag2, boolean[] fulfilledCol) {
+        if (row == n) {
+            List<String> cur = new LinkedList<>();
+
+            for (StringBuilder sb : path) {
+                cur.add(sb.toString());
+            }
+
+            res.add(cur);
+            return;
+        }
+
         for (int col = 0; col < n; col++) {
-            if (!colUsed[col] && !diag1Used[col + row] && !diag2Used[n - 1 - row + col]) {
-                cur.get(row).setCharAt(col, 'Q');
-                //n - 1, 0 -> 0
-                //n - 2, 0 - > 1
-                //n - 1, 2 -> 2
-                colUsed[col] = true;
-                diag1Used[col + row] = true;
-                diag2Used[n - 1 - row + col] = true;
-                dfs(res, cur, row + 1, colUsed, diag1Used, diag2Used, n);
-                cur.get(row).setCharAt(col, '.');
-                colUsed[col] = false;
-                diag1Used[col + row] = false;
-                diag2Used[n - 1 - row + col] = false;
+            if (!diag1.contains(row + col) && !diag2.contains(col - row) && !fulfilledCol[col]) {
+                path.get(row).setCharAt(col, 'Q');
+                fulfilledCol[col] = true;
+                diag1.add(row + col);
+                diag2.add(col - row);
+                backtracking(row + 1, n, path, diag1, diag2, fulfilledCol);
+                fulfilledCol[col] = false;
+                diag1.remove(row + col);
+                diag2.remove(col - row);
+                path.get(row).setCharAt(col, '.');
             }
         }
+
     }
 }

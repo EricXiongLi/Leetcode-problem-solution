e97c43387993287ed0937079db02d508657ba7a5
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-23 00:00:00 +0800

    Time: 2 ms (92.61%), Space: 45 MB (22.66%) - LeetHub

---
+++ 1087-brace-expansion/1087-brace-expansion.java
@@ -0,0 +1,45 @@
+class Solution {
+    List<List<Character>> options = new ArrayList<>();
+    List<String> res = new ArrayList<>();
+
+    public void generateOptions(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            List<Character> cur = new ArrayList<>();
+
+            if (Character.isLetter(c)) {
+                cur.add(c);
+            } else if (c == '{') {
+                while (s.charAt(i) != '}') {
+                    if (Character.isLetter(s.charAt(i))) {
+                        cur.add(s.charAt(i));
+                    }
+                    i++;
+                }
+            }
+
+            Collections.sort(cur);
+            options.add(cur);
+        }
+    }
+
+    public String[] expand(String s) {
+        generateOptions(s);
+        StringBuilder path = new StringBuilder();
+        backtracking(s, 0, path);
+        return res.toArray(new String[0]);
+    }
+
+    public void backtracking(String s, int pos, StringBuilder path) {
+        if (pos == options.size()) {
+            res.add(path.toString());
+            return;
+        }
+        for (char option : options.get(pos)) {
+            path.append(option);
+            backtracking(s, pos + 1, path);
+            path.deleteCharAt(path.length() - 1);
+        }
+    }
+    
+}

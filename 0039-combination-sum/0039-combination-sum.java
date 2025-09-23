92d02b94f854e87cc102dde99c04d4a6a3a26337
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-24 04:54:06 +0800

    Time: 2 ms (83.98%), Space: 45.4 MB (5.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,31 @@
 class Solution {
-
-    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        backtracking(candidates, target, 0, 0, new LinkedList<Integer>());
+        List<List<Integer>> res = new ArrayList<>();
+
+        Deque<Integer> path = new LinkedList<>();
+        int n = candidates.length;
+        backtracking(0, n - 1, path, res, candidates, target);
         return res;
     }
 
-    public void backtracking(int[] candidates, int target, int i, int curSum, Deque<Integer> curList)  {
-        if (curSum == target) {
-            res.add(new ArrayList<Integer>(curList));
+    public void backtracking(int start, int n, Deque<Integer> path, List<List<Integer>> res, int[] candidates, int target) {
+        //option: discard candidate[i] or not
+        if (target == 0) {
+            res.add(new ArrayList<Integer>(path));
             return;
         }
-
-        for (int j = i; j < candidates.length; j++) {
-            curSum += candidates[j];
-            curList.push(candidates[j]);
-            if (curSum <= target) {
-                backtracking(candidates, target, j, curSum, curList);
-            }
-            curSum -= candidates[j];
-            curList.pop();
-
-            //
+        if (start > n || target < 0 || target == 1) {
+            return;
         }
+        backtracking(start + 1, n, path, res, candidates, target);
+        
+        path.push(candidates[start]);
+        backtracking(start, n, path, res, candidates, target - candidates[start]);
+        path.pop();
+
+        
+        
+
+        
     }
 }

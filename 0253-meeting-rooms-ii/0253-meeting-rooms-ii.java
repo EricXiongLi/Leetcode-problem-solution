6ec254804924f1fe38ac730786786a798bf9c829
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-23 18:35:47 +0800

    Time: 11 ms (10.83%), Space: 45.7 MB (94.81%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,21 +1,21 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        List<int[]> list = new LinkedList<>();
+        List<int[]> list = new ArrayList<>();
         for (int[] interval : intervals) {
             list.add(new int[]{interval[0], 1});
             list.add(new int[]{interval[1], -1});
         }
 
-        Collections.sort(list, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
-
-        int res = 0;
+        //if open and close at the same time, prioritize closing
+        Collections.sort(list, (a, b) -> (a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]));
         int count = 0;
-
-        for (int[] item : list) {
-            count += item[1];
+        int res = 0;
+        for (int[] usage : list) {
+            count += usage[1];
             res = Math.max(res, count);
         }
 
         return res;
+        
     }
 }

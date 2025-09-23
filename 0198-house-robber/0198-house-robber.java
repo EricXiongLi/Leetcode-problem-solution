89373c033154cdfa17d2ca2a0fce496a2f7a29ee
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-24 00:29:25 +0800

    Time: 0 ms (100%), Space: 41.6 MB (8.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
-        //dp[i]: the max profit within [0...i]
-        //dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        //dp[i][0]: the max amount of money within nums[0...i], without robbing nums[i]
+        //dp[i][1]: the max  ...., with robbing nums[i]
 
+        //dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1])
+        //dp[i][1] = dp[i - 1][0] + nums[i]
         int n = nums.length;
-        if (n == 1) return nums[0];
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        //dp[-1][0] = 0, dp[-1][1] = 0
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
 
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = dp[i - 1][0] + nums[i];
         }
 
-        return dp[n - 1];
+        return Math.max(dp[n - 1][0], dp[n - 1][1]);
     }
 }

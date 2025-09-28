960b8070676c537da13bbacf34f6f8db8f7d1ee8
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-29 00:42:26 +0800

    Time: 14 ms (72.98%), Space: 44.6 MB (75.23%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,38 +1,42 @@
 class Solution {
     public int shortestPath(int[][] grid, int k) {
-        int[][] dirs = new int[][]{{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
-        Queue<int[]> q = new LinkedList<>();// row, col, kLeft
+        Queue<int[]> queue = new LinkedList<>();//[row, col, kLeft]
         int m = grid.length, n = grid[0].length;
-        if (k >= m + n - 3) return m + n - 2;
-        k = Math.min(m + n - 3, k);
+        if (m - 1 == 0 && n - 1 == 0) return 0;
         boolean[][][] visited = new boolean[m][n][k + 1];
-        q.offer(new int[]{0, 0, k});
+        if (k >= m + n - 3) return m + n - 2;
+        queue.offer(new int[]{0, 0, k});
         visited[0][0][k] = true;
-        int step = 0;
-        while (!q.isEmpty()) {
-            int sz = q.size();
-            for (int i = 0; i < sz; i++) {
-                int[] cur = q.poll();
-            int curRow = cur[0], curCol = cur[1], kLeft = cur[2];
-            if (curRow == m - 1 && curCol == n - 1) return step;
-            for (int[] dir : dirs) {
-                int newRow = curRow + dir[0];
-                int newCol = curCol + dir[1];
-                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
-                    if (visited[newRow][newCol][kLeft]) continue;
-                    if (grid[newRow][newCol] == 0) {
-                        q.offer(new int[]{newRow, newCol, kLeft});
-                        visited[newRow][newCol][kLeft] = true;
-                    } else {
-                        if (kLeft >= 1) {
-                            q.offer(new int[]{newRow, newCol, kLeft - 1});
-                            visited[newRow][newCol][kLeft - 1] = true;
+        int level = 0;
+        int[][] dirs = new int[][]{{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        while (!queue.isEmpty()) {
+            level++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.poll();
+
+                for (int[] dir : dirs) {
+                    int neiRow = dir[0] + cur[0], neiCol = dir[1] + cur[1];
+                    if (neiRow >= 0 && neiRow < m && neiCol >= 0 && neiCol < n) {
+                        if (visited[neiRow][neiCol][cur[2]]) continue;
+                        if (neiRow == m - 1 && neiCol == n - 1) {
+                            System.out.println("nei row, nei col: " + neiRow + neiCol);
+                            System.out.println("current k: " + cur[2]);
+                            return level;
+                        }
+                        if (grid[neiRow][neiCol] == 1) {
+                            if (cur[2] >= 1) {
+                                queue.offer(new int[]{neiRow, neiCol, cur[2] - 1});
+                                visited[neiRow][neiCol][cur[2] - 1] = true;
+                            }
+                        } else {
+                            queue.offer(new int[]{neiRow, neiCol, cur[2]});
+                            visited[neiRow][neiCol][cur[2]] = true;
                         }
                     }
+                    
                 }
             }
-            }
-            step++;
         }
         return -1;
     }

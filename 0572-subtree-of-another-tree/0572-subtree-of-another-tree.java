304c0a559fa570f68c3b89dfe39ca79a43c4ad5a
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-09-30 15:17:48 +0800

    Time: 1 ms (99.89%), Space: 44.5 MB (53.9%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,20 +15,20 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null && subRoot == null) return false;
-        if (root != null && subRoot == null) return true;
         if (root == null && subRoot != null) return false;
+        if (root != null && subRoot == null) return true;
+        if (root == null && subRoot == null) return true;
 
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot) || isSameTree(root, subRoot);
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot) || isSame(root, subRoot);
     }
 
-    public boolean isSameTree(TreeNode node1, TreeNode node2) {
+    public boolean isSame(TreeNode node1, TreeNode node2) {
         if (node1 == null && node2 == null) {
             return true;
-        } else if (node1 != null && node2 != null && node1.val == node2.val) {
-            return isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right);
+        } else if (node1 != null && node2 != null) {
+            return node1.val == node2.val && isSame(node1.left, node2.left) && isSame(node1.right, node2.right);
+        } else {
+            return false;
         }
-
-        return false;
     }
 }

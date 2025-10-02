3d1ee1352edf640c1a85ed55cf11655f272003a9
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-10-02 21:00:44 +0800

    Time: 6 ms (72.22%), Space: 44.4 MB (92.86%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,28 +1,35 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (String s : tokens) {
-            if (isOperator(s)) {
-                int x = stack.pop();
-                int y = stack.pop();
-                int res = operate(y, s, x);
-                stack.push(res);
+        Deque<String> stack = new LinkedList<>();
+
+        Set<String> ops = new HashSet<>();
+        ops.addAll(Arrays.asList("+", "-", "*", "/"));
+
+        for (int i = 0; i < tokens.length; i++) {
+            String token = tokens[i];
+            if (ops.contains(token)) {
+                String s2 = stack.pop();
+                String s1 = stack.pop();
+                int val = operate(s1, s2, token);
+                stack.push(String.valueOf(val));
             } else {
-                stack.push(Integer.valueOf(s));
+                stack.push(token);
             }
         }
-        return stack.pop();
+
+        return Integer.valueOf(stack.pop());
     }
-    
-    boolean isOperator(String s) {
-        return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
-    }
-    
-    int operate(int x, String s, int y) {
-        if (s.equals("+")) return x + y;
-        if (s.equals("-")) return x - y;
-        if (s.equals("*")) return x * y;
-        if (s.equals("/")) return x / y;
-        return 0;
+
+    public int operate(String s1, String s2, String ops) {
+        int val1 = Integer.valueOf(s1), val2 = Integer.valueOf(s2);
+        if (ops.equals("+")) {
+            return val1 + val2;
+        } else if (ops.equals("-")) {
+            return val1 - val2;
+        } else if (ops.equals("*")) {
+            return val1 * val2;
+        } else {
+            return val1 /val2;
+        }
     }
 }

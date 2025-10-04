58973acb0efad99e504b2eee91ada27771838a2b
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-10-04 19:49:20 +0800

    Time: 19 ms (80.24%), Space: 49.9 MB (5.09%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,39 +1,33 @@
-class Solution {    
+class Solution {
     public String removeDuplicates(String s, int k) {
-        Deque<Pair> stack = new ArrayDeque<>();
-        
-        for (char c : s.toCharArray()) {
-            if (stack.isEmpty() || stack.peek().ch != c) {
-                stack.push(new Pair(c, 1));
+        //[char - 'a', frequency]
+        Deque<int[]> stack = new LinkedList<>();
+
+        for (int i = 0 ; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (stack.isEmpty()) {
+                stack.push(new int[]{c - 'a', 1});
             } else {
-                stack.peek().count++;
-            }
-            if (stack.peek().count == k) {
-                stack.pop();
+                if (stack.peek()[0] == c - 'a') {
+                    if (stack.peek()[1] == k - 1) {
+                        for (int j = 0; j < k - 1; j++) {
+                            stack.pop();
+                        }
+                    } else {
+                        stack.push(new int[]{c - 'a', stack.peek()[1] + 1});
+                    }
+                } else {
+                    stack.push(new int[]{c - 'a', 1});
+                }
             }
         }
-        
-        StringBuilder sb = new StringBuilder();
+
+        StringBuilder res = new StringBuilder();
         while (!stack.isEmpty()) {
-            Pair pair = stack.pop();
-            for (int i = 0; i < pair.count; i++) {
-                sb.append(pair.ch);
-            }
+            res.append((char) (stack.pollLast()[0] + 'a'));
         }
-        return sb.reverse().toString();
-        
+
+        return res.toString();
     }
 }
-
-class Pair {
-        char ch;
-        int count;
-        
-        public Pair(char ch, int count) {
-            this.ch = ch;
-            this.count = count;
-        }
-}
-
-//tc: O(n)
-//sc: O(n)

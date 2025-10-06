6ecae6f2eec0e8a0ed2f0f555505619e0f13aa41
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-10-06 20:24:33 +0800

    Time: 52 ms (9.78%), Space: 73.1 MB (6.13%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,68 +1,35 @@
 class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+    Set<Integer> set = new HashSet<>();
     public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) return 0;
-         Map<Integer, Integer> map = new HashMap<>();
-        DSU dsu = new DSU(nums.length);
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) continue;
-            int greater = nums[i] + 1;
-            int smaller = nums[i] - 1;
-            if (map.containsKey(greater)) {
-                dsu.union(i, map.get(greater));
-            }
-            if (map.containsKey(smaller)) {
-                dsu.union(i, map.get(smaller));
-            }
-            map.put(nums[i], i);
+        
+
+        for (int num : nums) {
+            set.add(num);
         }
-        
-        return dsu.findMax();
-    }
-        
-        
-}
+        int res = 0;
+
+        for (int num : set) {
+            res = Math.max(res, count(num));
+        }
+
+        return res;
 
 
-class DSU {
-    int[] parent;
-    int[] size;
-    
-    public DSU(int n) {
-        parent = new int[n];
-        size = new int[n];
-        Arrays.fill(size, 1);
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+        
+    }
+
+    public int count(int num) {
+        if (map.containsKey(num)) {
+            return map.get(num);
         }
-    }
-    
-    public int find(int x) {
-        int parentX = parent[x];
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
-        } 
-        return parent[x];
-    }
-    
-    public void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX == rootY) return;
-        if (size[rootX] > size[rootY]) {
-            parent[rootY] = rootX;
-            size[rootX] += size[rootY];
-        } else {
-            parent[rootX] = rootY;
-            size[rootY] += size[rootX];
+        if (!set.contains(num)) {
+            map.put(num, 0);
+            return 0;
         }
+        int cnt;
+        cnt = count(num - 1) + 1;
+        map.put(num, cnt);
+        return cnt;
     }
-    
-    public int findMax() {
-        int max = 1;
-        for (int i : size) {
-            max = Math.max(i, max);
-        }
-        return max;
-    }
-    
 }

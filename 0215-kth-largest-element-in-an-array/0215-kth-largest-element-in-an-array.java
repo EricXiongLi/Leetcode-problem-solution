66928323c88a42a592a6f5e981ac161d782d5f4c
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-10-21 23:39:31 +0800

    Time: 33 ms (72.25%), Space: 61.7 MB (23.14%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,31 +1,28 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        List<Integer> list = new ArrayList<>();
-        for (int num : nums) {
-            list.add(num);
-        }
-        return quickselect(list, nums.length + 1 - k);
+        int n = nums.length;
+        List<Integer> numsList = new ArrayList<>();
+for (int num : nums) {
+    numsList.add(num);
+}
+return quickselect(numsList, n - k + 1);
     }
 
-
-    public int quickselect(List<Integer> list, int k) {
-        List<Integer> less = new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
-        List<Integer> greater = new ArrayList<>();
-
-        int pivotId = new Random().nextInt(list.size());
-        int pivot = list.get(pivotId);
-        for (int num : list) {
-            if (num < pivot) {
-                less.add(num);
-            } else if (num == pivot) {
+    public int quickselect(List<Integer> nums, int k) {
+        List<Integer> less = new ArrayList<Integer>(), equal = new ArrayList<>(), greater = new ArrayList<>();
+        int random = new Random().nextInt(nums.size());
+        int pivot = nums.get(random);
+        
+        for (int num : nums) {
+            if (num == pivot) {
                 equal.add(num);
+            } else if (num < pivot) {
+                less.add(num);
             } else {
                 greater.add(num);
             }
         }
-
-        if (less.size() >= k) {
+        if (k <= less.size()) {
             return quickselect(less, k);
         } else if (k > less.size() + equal.size()) {
             return quickselect(greater, k - less.size() - equal.size());
@@ -33,4 +30,5 @@ class Solution {
             return pivot;
         }
     }
+    
 }

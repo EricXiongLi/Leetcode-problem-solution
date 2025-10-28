e89b9de3f2813b18d09143307aba8475ed6b7ecc
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-10-29 06:11:42 +0800

    Time: 6 ms (98.43%), Space: 44.9 MB (38.43%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,38 @@
+class Solution {
+    class Word implements Comparable<Word> {
+        private String word;
+        private int count;
+
+        public Word(String word, int count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        public int compareTo(Word other) {
+            if (this.count == other.count) {
+                return this.word.compareTo(other.word);
+            }
+            return other.count - this.count;
+        }
+    }
+
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> cnt = new HashMap<>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+        }
+
+        List<Word> candidates = new ArrayList<>();
+        for (var entry : cnt.entrySet()) {
+            candidates.add(new Word(entry.getKey(), entry.getValue()));
+        }
+
+        Queue<Word> h = new PriorityQueue<>(candidates);
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            res.add(h.poll().word);
+        }
+        return res;
+
+    }
+}

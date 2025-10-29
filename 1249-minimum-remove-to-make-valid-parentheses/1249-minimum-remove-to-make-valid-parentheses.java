08c8e2e40c4c5798f83c80d7c95ed04fcfc390a8
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-10-29 23:44:13 +0800

    Time: 19 ms (60.89%), Space: 45.8 MB (32.23%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,40 +1,35 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        int left = 0, right = 0;
-        int full = 0;
+        Set<Integer> indexToRemove = new HashSet<>();
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == ')') {
-                right++;
-                if (left >= right) {
-                    full++;
-                    left--;
-                }
-                right--;
-            } else if (c == '('){
-                left++;
-            }
-        }
-        left = 0;
-        right = 0;
-        System.out.println(full);
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+
             if (c == '(') {
-                if (left < full) {
-                    res.append(c);
-                    left++;
-                }
+                stack.push(i);
             } else if (c == ')') {
-                if (right < full && right < left) {
-                    res.append(c);
-                    right++;
+                if (stack.isEmpty()) {
+                    indexToRemove.add(i);
+                } else {
+                    stack.pop();
                 }
-            } else {
-                res.append(c);
             }
         }
+
+        while (!stack.isEmpty()) {
+            indexToRemove.add(stack.pop());
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexToRemove.contains(i)) {
+                res.append(s.charAt(i));
+            }
+        }
+
         return res.toString();
     }
 }

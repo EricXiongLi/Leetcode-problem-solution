65257cb252cdc79fd034439c1a6d1d1dbbf7d07a
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-10-29 23:32:20 +0800

    Time: 2 ms (97.54%), Space: 42.2 MB (15.54%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack = new LinkedList<>();
+        Deque<Character> stack = new ArrayDeque<>();
 
         for (char c : s.toCharArray()) {
-                if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                    stack.pop();
-                } else if (c == ']' && !stack.isEmpty() && stack.peek() == 
-                '[') {
-                    stack.pop();
-                } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                    stack.pop();
-                } else {
-                    stack.push(c);
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
                 }
-            
+            }
         }
 
         return stack.isEmpty();

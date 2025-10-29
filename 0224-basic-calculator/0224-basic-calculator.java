8ea8c08224ddf5e3008c777ac5b29b32492e0a2f
Shawn Lee <91139789+EricXiongLi@users.noreply.github.com>
2025-10-29 19:26:28 +0800

    Time: 11 ms (52.39%), Space: 47 MB (7.27%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,48 +1,33 @@
 class Solution {
+    int idx = 0;
     public int calculate(String s) {
-        Map<Integer, Integer> nextBracket = new HashMap();
-        Stack<Integer> stack = new Stack();
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(i);
-            } else if (s.charAt(i) == ')') {
-                nextBracket.put(stack.pop(), i);
-            }
-        }
-        
-        return (int)calculate(s, 0, s.length() - 1, nextBracket);
-    }
-    
-    public long calculate(String s, int start, int end, Map<Integer, Integer> nextBracket) {
-        if (start == end) {
-            return (int)(s.charAt(start) - '0');
-        }
-        long sum = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        char op = '+';
         int num = 0;
-        //plus or minus
-        int flag = 1;
-        for (int i = start; i <= end; i++) {
-            char c = s.charAt(i);
-            if (c >= '0' && c <= '9') {
-                while (i <= end && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-                    num = 10 * num + (int)(s.charAt(i) - '0');
-                    i++;
-                }
-                i--;
-                sum = sum + flag * num;
-                num = 0;
-                flag = 1;
-            } else if (c == '-') {
-                flag = -1;
-            } else if (c == '+') {
-                continue;
-            } else if (c == '(') {
-                sum += flag * calculate(s, i + 1, nextBracket.get(i) - 1, nextBracket);
-                flag = 1;
-                i = nextBracket.get(i);
+        while (idx < s.length()) {
+            char ch = s.charAt(idx++);
+            if (ch >= '0' && ch <= '9') {
+                num = 10 * num + (ch - '0');
             }
+            if (ch == '(') {
+                num = calculate(s);
+            }
+            if (idx >= s.length() || ch == '+' || ch == '-' || ch == ')') {
+                if (op == '+') {
+                    stack.push(num);
+                } else if (op == '-') {
+                    stack.push(-num);
+                }
+                op = ch;
+                num = 0;
+            } 
+            
+            
+
+            if (ch == ')') break;
         }
-        return sum;
+
+        return stack.stream().mapToInt(i -> i).sum();
     }
 }
